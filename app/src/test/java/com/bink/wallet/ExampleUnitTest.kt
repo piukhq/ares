40363f5ab3385d52ae385d87152fde0ee9dc7192
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-07-16 17:26:21 +0300

    AB20-37 Started implementing unit tests for service registration

--- app/src/test/java/com/bink/wallet/ExampleUnitTest.kt
+++ app/src/test/java/com/bink/wallet/ExampleUnitTest.kt
@@ -1,17 +1,27 @@
 package com.bink.wallet
 
+import com.bink.wallet.network.ApiService
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.Mockito
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+class LoginUnitTest {
+
+    lateinit var apiService: ApiService
+
+    @Before
+    fun setUp(){
+        apiService = Mockito.mock(ApiService::class.java)
     }
+
+    @Test
+    fun registerCustomer(){
+        Mockito.`when`(apiService.registerCustomer())
+    }
+
 }
