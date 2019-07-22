88836a6982d2638bedd1a6acae41dfd937ae13cb
Teodora Popescu <tpopescu@bink.com>
2019-07-22 14:00:19 +0300

    AB20-44 added the recycler view and linked the fragment to the main activity

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
@@ -1,7 +1,12 @@
 package com.bink.wallet.scenes.loyalty_wallet;
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bink.wallet.R
 
 interface LoyaltyWalletDisplayLogic {
     fun displaySomething(viewModel: LoyaltyWalletModels.Something.ViewModel)
@@ -12,31 +17,16 @@ class LoyaltyWalletFragment : Fragment(), LoyaltyWalletDisplayLogic {
     lateinit var interactor: LoyaltyWalletBusinessLogic
     lateinit var router: ILoyaltyWalletRouter
 
-    init {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_loyalty_wallet, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         setup()
     }
 
     // Object lifecycle
-
-    override fun onStart() {
-        super.onStart()
-        // Called when the Fragment is visible to the user.
-    }
-
-    override fun onStop() {
-        super.onStop()
-        // Called when the Fragment is no longer started.
-    }
-
-    override fun onPause() {
-        super.onPause()
-        // Called when the Fragment is no longer resumed.
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // Called when the fragment is visible to the user and actively running.
-    }
 
     private fun setup() {
         // Setup the interactor, presenter, router and wire everything together
