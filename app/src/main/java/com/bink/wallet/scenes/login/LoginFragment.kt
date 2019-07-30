f6f3bd4c842f5a5b9506aa0a03cecb88fe3035bf
tpopescu <tpopescu@bink.com>
2019-07-30 17:16:33 +0300

    AB20-37 Updates the data flow to match SOLID principles.

--- app/src/main/java/com/bink/wallet/scenes/login/LoginFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/login/LoginFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import com.bink.wallet.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginFragment : Fragment() {
 
     private val viewModel: LoginViewModel by viewModel()
+    private val loginData = MutableLiveData<LoginBody>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +25,9 @@ class LoginFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel.loginData.observe(this, Observer {
-//            findNavController().navigate(R.id.whatever_route)
+        loginData.observe(this, Observer {
+            //            findNavController().navigate(R.id.whatever_route)
         })
+        viewModel.auth(loginData)
     }
 }
--- app/src/main/java/com/bink/wallet/scenes/login/LoginViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/login/LoginViewModel.kt
@@ -1,8 +1,20 @@
 package com.bink.wallet.scenes.login
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel constructor(private val loginRepository: LoginRepository) : ViewModel() {
-    var loginData: LiveData<LoginBody> = loginRepository.doAuthenticationWork()
+    //TODO Change email when login api is provided
+    fun auth(liveData: MutableLiveData<LoginBody>) {
+        loginRepository.doAuthenticationWork(
+            LoginResponse(
+                LoginBody(
+                    System.currentTimeMillis() / 1000,
+                    "Bink20iteration1@testbink.com",
+                    0.0,
+                    12.345
+                )
+            ), liveData
+        )
+    }
 }
