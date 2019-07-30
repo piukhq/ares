54d6c0d0f782a6cd39d56cb2bec20cc49c3bf484
tpopescu <tpopescu@bink.com>
2019-07-30 17:37:20 +0300

    AB20-37 Updates the basic unit test.

--- app/src/main/java/com/bink/wallet/scenes/login/LoginViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/login/LoginViewModel.kt
@@ -1,11 +1,14 @@
 package com.bink.wallet.scenes.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel constructor(private val loginRepository: LoginRepository) : ViewModel() {
+    var loginData: LiveData<LoginBody> = MutableLiveData<LoginBody>()
     //TODO Change email when login api is provided
     fun auth(liveData: MutableLiveData<LoginBody>) {
+        loginData = liveData
         loginRepository.doAuthenticationWork(
             LoginResponse(
                 LoginBody(
