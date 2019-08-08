06466f744ad5b9ab8dcfd8c69762fa7726a49263
Teodora Popescu <tposescu@bink.com>
2019-08-08 10:50:48 +0300

    AB20-15-Header Add header style/theme depending on version to match design Add settings button to main fragment toolbar

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
@@ -2,9 +2,8 @@ package com.bink.wallet.scenes.loyalty_wallet;
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -12,9 +11,11 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bink.wallet.R
+import com.bink.wallet.databinding.FragmentLoyaltyWalletBinding
 import com.bink.wallet.scenes.loyalty_wallet.RecyclerItemTouchHelper.RecyclerItemTouchHelperListener
 import kotlinx.android.synthetic.main.fragment_loyalty_wallet.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class LoyaltyWalletFragment : Fragment() {
     companion object {
@@ -22,17 +23,24 @@ class LoyaltyWalletFragment : Fragment() {
     }
 
     private var TAG = LoyaltyWalletFragment::class.simpleName
+    private lateinit var binding: FragmentLoyaltyWalletBinding
 
     private val viewModel: LoyaltyViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_loyalty_wallet, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_loyalty_wallet, container, false )
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         super.onActivityCreated(savedInstanceState)
 
         val listener: RecyclerItemTouchHelperListener = object :
@@ -52,19 +60,24 @@ class LoyaltyWalletFragment : Fragment() {
         }
 
         viewModel.deleteCard.observe(this, Observer {
-
             Log.e("Tag", "Deleted!")
 
         })
 
+        activity?.let {
+            it.setActionBar(binding.toolbar)
+            it.actionBar?.setDisplayShowTitleEnabled(false)
+        }
+
         viewModel.fetchMembershipCards()
+
 
         viewModel.membershipCardData.observe(this, Observer {
             loyalty_wallet_list.apply {
                 layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
                 adapter = LoyaltyWalletAdapter(it)
 
-                var helperListener = RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, listener)
+                val helperListener = RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, listener)
 
                 ItemTouchHelper(helperListener).attachToRecyclerView(this)
                 ItemTouchHelper(RecyclerItemTouchHelper(0, ItemTouchHelper.RIGHT, listener)).attachToRecyclerView(this)
@@ -80,6 +93,4 @@ class LoyaltyWalletFragment : Fragment() {
             }
         })
     }
-
-
 }
