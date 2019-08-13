aa1af975d374931416f1ee58a184ddb01c6675d0
Teodora Popescu <tposescu@bink.com>
2019-08-13 14:13:04 +0300

    Extend BaseFragment where needed such that the back functionality behaves accordingly

--- app/src/main/java/com/bink/wallet/scenes/add_join/AddJoinFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/add_join/AddJoinFragment.kt
@@ -1,38 +1,25 @@
 package com.bink.wallet.scenes.add_join
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.AddJoinFragmentBinding
 import kotlinx.android.synthetic.main.add_join_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AddJoinFragment : Fragment() {
+class AddJoinFragment : BaseFragment<AddJoinViewModel, AddJoinFragmentBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.add_join_fragment
 
     val args: AddJoinFragmentArgs by navArgs()
 
-    private val viewModel: AddJoinViewModel by viewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val addJoinBinding: AddJoinFragmentBinding =
-            DataBindingUtil.inflate(inflater, R.layout.add_join_fragment, container, false)
-        val currentMembershipPlan = args.currentMembershipPlan
-        addJoinBinding.item = currentMembershipPlan
-
-
-        return addJoinBinding.root
-    }
+    override val viewModel: AddJoinViewModel by viewModel()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val currentMembershipPlan = args.currentMembershipPlan
+        binding.item = currentMembershipPlan
         if (args.currentMembershipPlan.feature_set?.linking_support?.filter { it == "REGISTRATION" }?.size!! == 0) {
             add_join_view_image.setImageDrawable(context?.getDrawable(R.drawable.ic_icons_svl_view_inactive))
             add_join_view_description.text = getString(R.string.add_join_inactive_view_description)
