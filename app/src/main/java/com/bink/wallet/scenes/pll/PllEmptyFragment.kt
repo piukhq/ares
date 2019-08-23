2aa1de033eff1eb8fdc1f16b0ad1941f48d0631d
Mara Savan <msavan@bink.com>
2019-08-23 17:32:48 +0300

    AB20-111 Created the design for the screen

---
+++ app/src/main/java/com/bink/wallet/scenes/pll/PllEmptyFragment.kt
@@ -0,0 +1,20 @@
+package com.bink.wallet.scenes.pll
+
+/*
+class PllEmptyFragment : BaseFragment<PllViewModel, FragmentPllEmptyBindingImpl>(){
+
+    override val layoutRes: Int
+        get() = R.layout.fragment_pll_empty
+
+    override val viewModel: PllViewModel by viewModel()
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.toolbar.setNavigationIcon(R.drawable.ic_back)
+        binding.toolbar.setNavigationOnClickListener {
+            activity?.onBackPressed()
+    }
+
+}*/
