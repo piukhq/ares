e6ed4e7f87083cecb38f32a46ec32ee21b08d9a4
Teodora Popescu <tposescu@bink.com>
2019-08-28 13:50:06 +0300

    AB20-98-LCD-Loyalty-Card Create reusable loyalty card header Integrate header into app

--- app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsViewModel.kt
@@ -3,11 +3,13 @@ package com.bink.wallet.scenes.loyalty_details
 import androidx.lifecycle.MutableLiveData
 import com.bink.wallet.BaseViewModel
 import com.bink.wallet.scenes.browse_brands.model.MembershipPlan
+import com.bink.wallet.scenes.loyalty_wallet.model.MembershipCard
 
 class LoyaltyCardDetailsViewModel(private val repository: LoyaltyCardDetailsRepository) :
     BaseViewModel() {
     var tiles = MutableLiveData<List<String>>()
     var membershipPlan = MutableLiveData<MembershipPlan>()
+    var membershipCard = MutableLiveData<MembershipCard>()
     var deletedCard = MutableLiveData<String>()
     var deleteError = MutableLiveData<String>()
 
