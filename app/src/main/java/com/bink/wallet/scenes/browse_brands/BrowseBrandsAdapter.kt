e9bc71a060dacf51279ea9c9e60c2300fe1e53b3
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-09-16 12:15:35 +0300

    AB20-147 Code optimisation

--- app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsAdapter.kt
+++ app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsAdapter.kt
@@ -4,19 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bink.wallet.R
 import com.bink.wallet.databinding.BrandListItemBinding
 import com.bink.wallet.model.response.membership_plan.MembershipPlan
 import com.bink.wallet.utils.enums.CardType
 
 
 class BrowseBrandsAdapter(
-    val brands: List<MembershipPlan>,
+    val brands: List<Pair<String?, MembershipPlan>>,
     val itemClickListener: (MembershipPlan) -> Unit = {}
 ) :
-    RecyclerView.Adapter<BrowseBrandsAdapter.MyViewHolder>() {
+    RecyclerView.Adapter<BrowseBrandsAdapter.BrandsViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrandsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = BrandListItemBinding.inflate(inflater)
         binding.apply {
@@ -26,39 +25,35 @@ class BrowseBrandsAdapter(
                 }
             }
         }
-        return MyViewHolder(binding)
+        return BrandsViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        if (position > 0) {
-            if (brands[position - 1].feature_set?.card_type == 2 &&
-                brands[position].feature_set?.card_type != 2
-            ) {
-                holder.binding.sectionTitle.text =
-                    holder.binding.sectionTitle.context.getString(R.string.popular_text)
-            } else {
-                holder.binding.sectionTitle.visibility = View.GONE
-            }
-        }
+    override fun onBindViewHolder(holder: BrandsViewHolder, position: Int) {
         brands[position].let { holder.bind(it) }
     }
 
-    override fun getItemCount(): Int {
-        return brands.size
-    }
+    override fun getItemCount(): Int = brands.size
 
-    class MyViewHolder(val binding: BrandListItemBinding) :
+    class BrandsViewHolder(val binding: BrandListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: MembershipPlan) {
-            binding.item = item
+        fun bind(item: Pair<String?, MembershipPlan>) {
+
+            if (item.first != null) {
+                binding.sectionTitle.text = item.first
+                binding.sectionTitle.visibility = View.VISIBLE
+            } else {
+                binding.sectionTitle.visibility = View.GONE
+            }
+
+            binding.item = item.second
             binding.executePendingBindings()
 
-            if (item.getCardType() == CardType.PLL) {
-                binding.browseBrandsDescription.visibility = View.VISIBLE
-            } else {
-                binding.browseBrandsDescription.visibility = View.INVISIBLE
-            }
+            binding.browseBrandsDescription.visibility =
+                when (item.second.getCardType() == CardType.PLL) {
+                    true -> View.VISIBLE
+                    else -> View.INVISIBLE
+                }
         }
     }
 }
--- app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsFragment.kt
@@ -28,11 +28,25 @@ class BrowseBrandsFragment : BaseFragment<BrowseBrandsViewModel, BrowseBrandsFra
         super.onActivityCreated(savedInstanceState)
 
         arguments?.let { brandsList ->
-            val plans = BrowseBrandsFragmentArgs.fromBundle(brandsList).membershipPlans
+            var plans = BrowseBrandsFragmentArgs.fromBundle(brandsList).membershipPlans
 
-            val plansList =
+            val plansList = ArrayList<Pair<String?, MembershipPlan>>()
+
+            plans =
                 plans.sortedWith(compareByDescending<MembershipPlan> { it.feature_set?.card_type }
-                    .thenBy { it.account?.company_name })
+                    .thenBy { it.account?.company_name }).toTypedArray()
+
+            plansList.add(Pair(getString(R.string.pll_text), plans[0]))
+
+            for (position in 1 until plans.size) {
+                if (plans[position - 1].feature_set?.card_type == 2 &&
+                    plans[position].feature_set?.card_type != 2
+                ) {
+                    plansList.add(Pair(getString(R.string.popular_text), plans[position]))
+                } else {
+                    plansList.add(Pair(null, plans[position]))
+                }
+            }
 
             browse_brands_container.apply {
                 layoutManager = GridLayoutManager(activity, 1)
