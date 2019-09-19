1038e8d339a96c09c16ebde9ff6942218ba15e12
Teodora Popescu <tposescu@bink.com>
2019-09-19 14:45:34 +0300

    AB20-168-Create-payment-card-item-layout Start linking data

--- app/src/main/java/com/bink/wallet/scenes/pll/PllViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/pll/PllViewModel.kt
@@ -1,6 +1,13 @@
 package com.bink.wallet.scenes.pll
 
+import androidx.lifecycle.MutableLiveData
 import com.bink.wallet.BaseViewModel
+import com.bink.wallet.model.response.membership_card.MembershipCard
+import com.bink.wallet.model.response.membership_plan.MembershipPlan
+import com.bink.wallet.model.response.payment_card.PaymentCard
 
 class PllViewModel: BaseViewModel() {
+    var membershipCard = MutableLiveData<MembershipCard>()
+    var membershipPlan = MutableLiveData<MembershipPlan>()
+    var paymentCards = MutableLiveData<PaymentCard>()
 }
