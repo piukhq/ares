6092850eaf290ca3f4efe0acc7bc5183b964f3a3
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-09-19 08:26:01 +0300

    AB20-164 Changed the text of title and button

--- app/src/main/java/com/bink/wallet/scenes/add_auth_enrol/AddAuthFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/add_auth_enrol/AddAuthFragment.kt
@@ -68,8 +68,6 @@ class AddAuthFragment : BaseFragment<AddAuthViewModel, AddAuthFragmentBinding>()
                 currentMembershipPlan.feature_set.has_points == true &&
                 currentMembershipPlan.feature_set.transactions_available != null
             ) {
-                binding.titleAddAuthText.text = getString(R.string.log_in_text)
-                binding.addCardButton.text = getString(R.string.log_in_text)
                 if (currentMembershipPlan.feature_set.transactions_available == true) {
                     binding.descriptionAddAuth.text = getString(
                         R.string.log_in_transaction_available,
@@ -89,8 +87,6 @@ class AddAuthFragment : BaseFragment<AddAuthViewModel, AddAuthFragmentBinding>()
                     currentMembershipCard
                 ) == LoginStatus.STATUS_LOGIN_FAILED
             ) {
-                binding.titleAddAuthText.text = getString(R.string.log_in_text)
-                binding.addCardButton.text = getString(R.string.log_in_text)
                 binding.descriptionAddAuth.text = getString(
                     R.string.log_in_transaction_available,
                     currentMembershipPlan.account?.plan_name_card
