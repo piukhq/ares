ace68f9ae4962e060d829eac251d6f504b60909c
Philip Arnold <parnold@bink.com>
2019-09-19 09:24:44 +0100

    AB20-151 - fixes to C++ code

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -3,10 +3,11 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_bink_wallet_MainActivity_getNativeKey(JNIEnv *env, jobject instance, jstring jstring1) {
-    const char *nativeString1 = (*env)->GetStringUTFChars(jstring1, 0, NULL);
-    if (strstr(nativeString1, "staging")) {
+    const char *nativeString = (*env)->GetStringUTFChars(env, jstring1, 0);
+    if (strstr(nativeString, "staging")) {
         return (*env)->NewStringUTF(env,
                                     "dlhrQWY2S0ZUVWNuUkJ0RFJ1cWRYYWRhSHA1Rk9QZFU1Z25XMlZiTkh1WDVONnNOTGU");
     }
-    return (*env)->  NewStringUTF(env, "UUZVa0tHblc4U0VDWXdLUlZXcXJRS2xhb2dOYWtLNElxRXVuMDlHb0ZSQmxoeWltc3c");
+    return (*env)->  NewStringUTF(env,
+            "UUZVa0tHblc4U0VDWXdLUlZXcXJRS2xhb2dOYWtLNElxRXVuMDlHb0ZSQmxoeWltc3c");
 }
