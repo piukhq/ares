05f6e153f8092e36b336bc4af27e1564ff84c4f6
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-09-19 22:26:29 +0300

    View unit testing

--- app/src/androidTest/java/com/bink/wallet/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bink/wallet/ExampleInstrumentedTest.kt
@@ -1,24 +1,34 @@
 package com.bink.wallet
 
-import androidx.test.InstrumentationRegistry
+
+import androidx.lifecycle.MutableLiveData
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-
-import org.junit.Test
+import com.bink.wallet.model.response.membership_card.MembershipCard
+import com.bink.wallet.model.response.membership_plan.MembershipPlan
+import com.bink.wallet.scenes.loyalty_wallet.LoyaltyViewModel
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+//import org.mockito.Mockito.mock
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.bink.wallet", appContext.packageName)
+@LargeTest
+class LoyaltyWalletViewTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, true)
+
+    private lateinit var viewModel: LoyaltyViewModel
+    private val membershipCards = MutableLiveData<MembershipCard>()
+    private val membershipPlans = MutableLiveData<MembershipPlan>()
+
+    @Before
+    fun init() {
+//        viewModel = mock(LoyaltyViewModel::class.java)
+
+
     }
 }
