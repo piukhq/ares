c6f4628e36eda0c0af5481ab41fb00973701acd5
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-09-27 12:17:21 +0300

    AB20-161 Changed barcode to membership_id

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/BarcodeViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/BarcodeViewModel.kt
@@ -2,11 +2,13 @@ package com.bink.wallet.scenes.loyalty_wallet
 
 import androidx.lifecycle.MutableLiveData
 import com.bink.wallet.BaseViewModel
+import com.bink.wallet.model.response.membership_card.MembershipCard
 import com.bink.wallet.model.response.membership_plan.MembershipPlan
 import com.bink.wallet.utils.BarcodeWrapper
 
 class BarcodeViewModel : BaseViewModel() {
     var membershipPlan = MutableLiveData<MembershipPlan>()
+    var membershipCard = MutableLiveData<MembershipCard>()
     var barcode = MutableLiveData<BarcodeWrapper>()
     var isBarcodeAvailable = MutableLiveData<Boolean>()
 }
