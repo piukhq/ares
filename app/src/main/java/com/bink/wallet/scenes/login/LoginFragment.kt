cc2dbf07ec761a23f0497ce413772aed5a36f64e
Phil Arnold <parnold@bink.com>
2019-10-02 09:20:30 +0100

    AB20-151 last observer updates

--- app/src/main/java/com/bink/wallet/scenes/login/LoginFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.LoginFragmentBinding
 import com.bink.wallet.utils.navigateIfAdded
+import com.bink.wallet.utils.observeNonNull
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import com.bink.wallet.utils.verifyAvailableNetwork
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -29,16 +30,16 @@ class LoginFragment : BaseFragment<LoginViewModel, LoginFragmentBinding>() {
         super.onActivityCreated(savedInstanceState)
 
         viewModel.retrieveStoredLoginData()
-        viewModel.loginData.observe(this, Observer {
+        viewModel.loginData.observeNonNull(this) {
             if (verifyAvailableNetwork(requireActivity())) {
                 viewModel.authenticate()
             } else {
                 showNoInternetConnectionDialog()
             }
-        })
+        }
 
-        viewModel.loginData.observe(this, Observer {
+        viewModel.loginData.observeNonNull(this) {
             findNavController().navigateIfAdded(this, R.id.global_to_home)
-        })
+        }
     }
 }
