e79e614a79a7028c0f7279b11ad7a3d3195eba51
Phil Arnold <parnold@bink.com>
2019-10-07 11:51:58 +0100

    AB20-90 Luhn validation & unit tests

--- app/src/main/java/com/bink/wallet/utils/PaymentCardUtils.kt
+++ app/src/main/java/com/bink/wallet/utils/PaymentCardUtils.kt
@@ -2,7 +2,6 @@ package com.bink.wallet.utils
 
 import com.bink.wallet.model.response.membership_card.MembershipCard
 import com.bink.wallet.model.response.payment_card.PaymentCard
-import com.bink.wallet.model.response.payment_card.PaymentMembershipCard
 
 fun PaymentCard.isLinkedToMembershipCard(membershipCard: MembershipCard) : Boolean {
     membership_cards?.forEach { paymentMembershipCard ->
@@ -17,16 +16,19 @@ fun PaymentCard.isLinkedToMembershipCard(membershipCard: MembershipCard) : Boole
 fun String.luhnValidation() : Boolean {
     val sanitizedInput = this.replace(" ", "")
     return when {
-        sanitizedInput.luhnValidPopulated() -> sanitizedInput.checksum() % 10 == 0
+        sanitizedInput.luhnLengthInvalid() -> false
+        sanitizedInput.luhnValidPopulated() -> sanitizedInput.luhnChecksum() % 10 == 0
         else -> false
     }
 }
 
 fun String.luhnValidPopulated() = this.all(Char::isDigit) && this.length > 1
 
-fun String.checksum() = this.addends().sum()
+fun String.luhnLengthInvalid() = !(this.length == 16 || this.length == 15)
 
-fun String.addends() = this.digits().mapIndexed { i, j ->
+fun String.luhnChecksum() = this.luhnMultiply().sum()
+
+fun String.luhnMultiply() = this.digits().mapIndexed { i, j ->
     when {
         (this.length - i + 1) % 2 == 0 -> j
         j >= 5 -> j * 2 - 9
--- app/src/test/java/com/bink/wallet/utils/PaymentCardUtilsTests.kt
+++ app/src/test/java/com/bink/wallet/utils/PaymentCardUtilsTests.kt
@@ -1,13 +1,41 @@
 package com.bink.wallet.utils
 
-import junit.framework.Assert.assertFalse
+import junit.framework.TestCase.assertTrue
+import junit.framework.TestCase.assertFalse
 import org.junit.Test
 
 class PaymentCardUtilsTests {
     @Test
     fun testLuhnEmptyCard() {
-        val cardNumber = ""
-        assertFalse(cardNumber.luhnValidation())
+        assertFalse("".luhnValidation())
     }
 
+    @Test
+    fun singleDigitStringsCannotBeValid() {
+        assertFalse("1".luhnValidation())
+    }
+
+    @Test
+    fun singleZeroIsInvalid() {
+        assertFalse("0".luhnValidation())
+    }
+
+    @Test
+    fun invalidWithNonDigit() {
+        assertFalse("055a 444 285".luhnValidation())
+    }
+
+    @Test
+    fun invalidLength() {
+        assertFalse("0555 1444 2285".luhnValidation())
+    }
+
+    @Test
+    fun validVisa() {
+        assertTrue("4242 4242 4242 4242".luhnValidation())
+    }
+    @Test
+    fun validAmEx() {
+        assertTrue("3400 00000 000009".luhnValidation())
+    }
 }
