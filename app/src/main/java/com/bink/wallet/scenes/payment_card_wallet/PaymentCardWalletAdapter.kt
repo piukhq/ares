1bb5f6190d6cc8cdea7d1164b504e00cebef02e0
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-10-10 18:05:08 +0300

    AB20-133 Resolved mr comment

--- app/src/main/java/com/bink/wallet/scenes/payment_card_wallet/PaymentCardWalletAdapter.kt
+++ app/src/main/java/com/bink/wallet/scenes/payment_card_wallet/PaymentCardWalletAdapter.kt
@@ -2,7 +2,9 @@ package com.bink.wallet.scenes.payment_card_wallet
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bink.wallet.R
 import com.bink.wallet.databinding.PaymentCardWalletItemBinding
 import com.bink.wallet.model.response.payment_card.PaymentCard
 
@@ -13,8 +15,12 @@ class PaymentCardWalletAdapter(
 ) : RecyclerView.Adapter<PaymentCardWalletAdapter.PaymentCardWalletHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentCardWalletHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = PaymentCardWalletItemBinding.inflate(inflater)
+        val binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.payment_card_wallet_item,
+            parent,
+            false
+        ) as PaymentCardWalletItemBinding
         return PaymentCardWalletHolder(
             binding
         )
