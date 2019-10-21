46df70e878f19b44f97835c147f5a17cf9891689
Teodora <tpopescu@bink.com>
2019-10-21 15:41:36 +0300

    AB20-239-Create Onboarding Fragment And Frame

---
+++ app/src/main/java/com/bink/wallet/scenes/onboarding/OnboardingFragment.kt
@@ -0,0 +1,25 @@
+package com.bink.wallet.scenes.onboarding
+
+import android.os.Bundle
+import com.bink.wallet.BaseFragment
+import com.bink.wallet.R
+import com.bink.wallet.databinding.OnboardingFragmentBinding
+import com.bink.wallet.utils.toolbar.FragmentToolbar
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class OnboardingFragment : BaseFragment<OnboardingViewModel, OnboardingFragmentBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.onboarding_fragment
+    override val viewModel: OnboardingViewModel by viewModel()
+
+    override fun builder(): FragmentToolbar {
+        return FragmentToolbar.Builder()
+            .with(binding.toolbar)
+            .shouldDisplayBack(requireActivity())
+            .build()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
