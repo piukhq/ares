68c75e04bd6e39ea99e5a4c7dcce91742a425b7d
Teodora <tpopescu@bink.com>
2019-10-22 15:01:43 +0300

    Add onboarding pager adapter

---
+++ app/src/main/java/com/bink/wallet/scenes/onboarding/OnboardingPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.bink.wallet.scenes.onboarding
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class OnboardingPagerAdapter(fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val ONBOARDING_PAGES_NUMBER = 3
+    private val onboardingFragments = mutableListOf<Fragment>()
+
+    override fun getItem(position: Int): Fragment  = onboardingFragments[position]
+
+    override fun getCount(): Int =  ONBOARDING_PAGES_NUMBER
+}
