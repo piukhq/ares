89630b7c6b29d9f77d62caf0942abb3271cc678d
Phil Arnold <parnold@bink.com>
2019-11-01 15:16:13 +0000

    AB20-249 revert validation issue changes

--- app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsFragment.kt
@@ -210,7 +210,7 @@ class LoyaltyCardDetailsFragment :
         setPointsModuleClickListener()
         setLinkModuleClickListener()
 
-        binding.footerDelete.setOnClickListener { footerView: View ->
+        binding.footerDelete.setOnClickListener { footerView ->
             val builder = AlertDialog.Builder(context)
             var dialog: AlertDialog? = null
             builder.setMessage(getString(R.string.delete_card_modal_body))
@@ -384,7 +384,7 @@ class LoyaltyCardDetailsFragment :
                     }
                     if (directions != null) {
                         findNavController().navigateIfAdded(this, directions)
-                    } else {}
+                    }
                 }
                 LinkStatus.STATUS_LINKABLE_NO_PAYMENT_CARDS_LINKED -> {
                     val directions =
