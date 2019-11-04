50abea7c2982170a24f5fb61184b48936832cd8f
Lucian Beresescu <lucian.beresescu@greppysystems.com>
2019-11-04 16:28:38 +0200

    AB20-125 Increased DB number

--- app/src/main/java/com/bink/wallet/data/BinkDatabase.kt
+++ app/src/main/java/com/bink/wallet/data/BinkDatabase.kt
@@ -11,7 +11,7 @@ import com.bink.wallet.model.response.payment_card.PaymentCard
 
 @Database(
     entities = [MembershipCard::class, MembershipPlan::class, LoginData::class, PaymentCard::class, BannerDisplay::class],
-    version = 10,
+    version = 12,
     exportSchema = false
 )
 @TypeConverters(
