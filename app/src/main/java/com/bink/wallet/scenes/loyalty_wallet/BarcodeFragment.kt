2b83a2fca625dc2a6e74e81b10324fbdf3973c77
Teodora <tpopescu@bink.com>
2019-11-07 15:24:45 +0200

    Handle API nullability for card number text on barcode screen

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/BarcodeFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/BarcodeFragment.kt
@@ -1,6 +1,7 @@
 package com.bink.wallet.scenes.loyalty_wallet
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
@@ -28,6 +29,12 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
             BarcodeFragmentArgs.fromBundle(it).apply {
                 viewModel.membershipPlan.value = currentMembershipPlan
                 viewModel.membershipCard.value = membershipCard
+                if(membershipCard.card?.membership_id.isNullOrEmpty()) {
+                    with(binding) {
+                        cardNumberLabel.visibility = View.GONE
+                        cardNumber.visibility = View.GONE
+                    }
+                }
                 viewModel.barcode.value = BarcodeWrapper(membershipCard)
                 viewModel.isBarcodeAvailable.value = !membershipCard.card?.barcode.isNullOrEmpty()
             }
