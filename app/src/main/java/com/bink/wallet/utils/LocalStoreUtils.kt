7d2d8386fd379f106fbe93a25ee34ce01af32374
Phil Arnold <parnold@bink.com>
2019-12-09 17:36:59 +0000

    AB20-444 code-tidy from review

--- app/src/main/java/com/bink/wallet/utils/LocalStoreUtils.kt
+++ app/src/main/java/com/bink/wallet/utils/LocalStoreUtils.kt
@@ -17,10 +17,11 @@ object LocalStoreUtils {
 
     fun setAppSharedPref(secretKey: String, secret: String) {
         try {
-            val editor = encryptedSharedPreferences.edit()
-            editor.putString(secretKey, secret)
-            editor.apply()
-            editor.commit()
+            encryptedSharedPreferences.edit().let {
+                it.putString(secretKey, secret)
+                it.apply()
+                it.commit()
+            }
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -50,9 +51,10 @@ object LocalStoreUtils {
     }
 
     fun clearPreferences() {
-        val editor = encryptedSharedPreferences.edit()
-        editor.clear()
-        editor.apply()
-        editor.commit()
+        encryptedSharedPreferences.edit().let {
+            it.clear()
+            it.apply()
+            it.commit()
+        }
     }
 }
