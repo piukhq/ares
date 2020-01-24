84b3bc09609ee0c367127bf3fa89e5f2633fe085
Phil Arnold <parnold@bink.com>
2020-01-24 16:55:17 +0000

    AB20-303 add production pin

--- app/src/main/java/com/bink/wallet/di/CertificatePins.kt
+++ app/src/main/java/com/bink/wallet/di/CertificatePins.kt
@@ -4,5 +4,6 @@ enum class CertificatePins(val domain: String, val hash: String) {
     // This class is "infinitely expandable" with extra domains & hashes,
     // so for updated keys we just have to add new entries... good, isn't it? 🙂
     DEBUG("api.dev.gb.bink.com", "sha256/35Y7j68+91YIgnJV8bQQfGfqwLP1OPnM6PZAjVw79P4="),
-    STAGING("api.staging.gb.bink.com", "sha256/z7/qT56fkSkCNT8b4CKoIyCuK7MEFytc63IzAyRgD1g=")
+    STAGING("api.staging.gb.bink.com", "sha256/z7/qT56fkSkCNT8b4CKoIyCuK7MEFytc63IzAyRgD1g="),
+    PROD("api.bink.com", "sha256/DbK9h+lZezU9L8K3TW8Wnmg917UqNzhYmOKsv0iK3us=")
 }
