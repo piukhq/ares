629c1fb515bfaee3957c757331a32ef6823d9803
Phil Arnold <parnold@bink.com>
2020-01-31 18:18:27 +0000

    AB20-487 handle no transactions or features set to false

--- app/src/main/java/com/bink/wallet/scenes/transactions_screen/TransactionsFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/transactions_screen/TransactionsFragment.kt
@@ -1,15 +1,18 @@
 package com.bink.wallet.scenes.transactions_screen
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.TransactionFragmentBinding
 import com.bink.wallet.modal.generic.GenericModalParameters
+import com.bink.wallet.utils.getElapsedTime
 import com.bink.wallet.utils.navigateIfAdded
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class TransactionsFragment : BaseFragment<TransactionViewModel, TransactionFragmentBinding>() {
     override fun builder(): FragmentToolbar {
@@ -52,15 +55,39 @@ class TransactionsFragment : BaseFragment<TransactionViewModel, TransactionFragm
 
         viewModel.membershipCard.observeForever {
             it.membership_transactions?.let { transactions ->
-                if (transactions.isEmpty()) {
-                    binding.pointsDescription.text = getString(R.string.no_transactions_text)
-                }
-                binding.transactionsList.apply {
-                    layoutManager = GridLayoutManager(activity, 1)
-                    adapter = TransactionAdapter(transactions)
+                if (transactions.isEmpty() ||
+                    viewModel.membershipPlan.value?.feature_set?.transactions_available != true
+                ) {
+                    with (binding) {
+                        pointsHistory.text =
+                            getString(R.string.points_history_not_available_title)
+                        pointsDescription.text =
+                            getString(
+                                R.string.points_history_not_available_description,
+                                viewModel.membershipPlan.value?.account?.plan_name
+                            )
+                        with (noTransactionsText) {
+                            val balance =
+                                viewModel.membershipCard.value?.balances?.first()
+                            val updateTime = balance?.updated_at
+                            val currentTime = Calendar.getInstance().timeInMillis / 1000
+                            updateTime?.let { time ->
+                                val timeSinceUpdate = currentTime - time
+                                visibility = View.VISIBLE
+                                text = getString(
+                                    R.string.points_history_not_available_period,
+                                    timeSinceUpdate.getElapsedTime(requireContext())
+                                )
+                            }
+                        }
+                    }
+                } else {
+                    binding.transactionsList.apply {
+                        layoutManager = GridLayoutManager(activity, 1)
+                        adapter = TransactionAdapter(transactions)
+                    }
                 }
             }
         }
     }
-
 }
