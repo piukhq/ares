e0afaac97c602bd3a8ad5e9403755dc51e04c330
Teodora <tpopescu@bink.com>
2020-02-12 16:55:08 +0200

    AB20-268-PLL-Unlinking-And-Linking-Behaviour Add code cleanup

--- app/src/main/java/com/bink/wallet/scenes/add_payment_card/AddPaymentCardViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/add_payment_card/AddPaymentCardViewModel.kt
@@ -6,7 +6,6 @@ import com.bink.wallet.modal.card_terms_and_conditions.AddPaymentCardRepository
 import com.bink.wallet.model.response.membership_card.MembershipCard
 import com.bink.wallet.model.response.membership_plan.MembershipPlan
 import com.bink.wallet.model.response.payment_card.PaymentCard
-import com.bink.wallet.model.response.payment_card.PaymentCardAdd
 
 class AddPaymentCardViewModel(private val repository: AddPaymentCardRepository) :
     BaseViewModel() {
@@ -16,15 +15,10 @@ class AddPaymentCardViewModel(private val repository: AddPaymentCardRepository) 
     val cardHolder = MutableLiveData<String>()
 
     var paymentCard = MutableLiveData<PaymentCard>()
-    var addedError = MutableLiveData<Throwable>()
-
+  
     var localMembershipCardData = MutableLiveData<List<MembershipCard>>()
     var localMembershipPlanData = MutableLiveData<List<MembershipPlan>>()
 
-    fun sendAddCard(card: PaymentCardAdd) {
-        addedError.value = null
-        repository.sendAddCard(card, paymentCard, addedError)
-    }
 
     fun fetchLocalMembershipCards() {
         repository.retrieveStoredMembershipCards(localMembershipCardData)
