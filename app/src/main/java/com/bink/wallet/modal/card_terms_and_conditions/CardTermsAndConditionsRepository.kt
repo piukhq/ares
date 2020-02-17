372552d558a82e29e6807d56cc427927554fcf1b
Connor <cmcfadden@bink.com>
2020-02-17 14:42:26 +0000

    Blocked Spreedly request from all environments apart from production, which also incuded updating the base_url within the release buildType to point to prod

--- app/src/main/java/com/bink/wallet/modal/card_terms_and_conditions/CardTermsAndConditionsRepository.kt
+++ app/src/main/java/com/bink/wallet/modal/card_terms_and_conditions/CardTermsAndConditionsRepository.kt
@@ -2,6 +2,7 @@ package com.bink.wallet.modal.card_terms_and_conditions
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.bink.wallet.BuildConfig
 import com.bink.wallet.data.MembershipCardDao
 import com.bink.wallet.data.MembershipPlanDao
 import com.bink.wallet.data.PaymentCardDao
@@ -34,47 +35,51 @@ class CardTermsAndConditionsRepository(
         error: MutableLiveData<Throwable>
     ) {
 
-        val spreedlyEnvironmentKey = LocalStoreUtils.getAppSharedPref(
-            LocalStoreUtils.KEY_SPREEDLY
-        )?.let {
-            it
-        }
+        // Here we send the users payment card to Spreedly before making a request to Binks API.
+        // This can only happen on release, so we have to guard it with the following condition.
+        // Another option would be to add an implementation of this class on the /release buildType.
+        // However, for this to work we'd have to add the same class across all buildTypes
+        // and remove the class from /main. Which seems massively overkill - if anything ever
+        // changes, we'll have to remember to update across the project.
+        if (BuildConfig.BUILD_TYPE == "release") {
+            val spreedlyEnvironmentKey = LocalStoreUtils.getAppSharedPref(
+                LocalStoreUtils.KEY_SPREEDLY
+            )?.let {
+                it
+            }
 
-        //todo safety checks
-        val spreedlyCreditCard = SpreedlyCreditCard(
-            cardNumber,
-            card.card.month!!,
-            card.card.year!!,
-            card.card.name_on_card!!
-        )
-        val spreedlyPaymentMethod = SpreedlyPaymentMethod(spreedlyCreditCard, "true")
-        val spreedlyPaymentCard = SpreedlyPaymentCard(spreedlyPaymentMethod)
-        CoroutineScope(Dispatchers.IO).launch {
-            val spreedlyRequest = spreedlyApiService.postPaymentCardToSpreedly(spreedlyPaymentCard, spreedlyEnvironmentKey!!)
-            withContext(Dispatchers.Main) {
-                try {
-                    val response = spreedlyRequest.await()
-                    card.card.token = response.transaction.payment_method.token
-                    card.card.fingerprint = response.transaction.payment_method.fingerprint
-                    card.card.first_six_digits =
-                        response.transaction.payment_method.first_six_digits
-                    card.card.last_four_digits =
-                        response.transaction.payment_method.last_four_digits
+            //todo safety checks
+            val spreedlyCreditCard = SpreedlyCreditCard(
+                cardNumber,
+                card.card.month!!,
+                card.card.year!!,
+                card.card.name_on_card!!
+            )
+            val spreedlyPaymentMethod = SpreedlyPaymentMethod(spreedlyCreditCard, "true")
+            val spreedlyPaymentCard = SpreedlyPaymentCard(spreedlyPaymentMethod)
+            CoroutineScope(Dispatchers.IO).launch {
+                val spreedlyRequest = spreedlyApiService.postPaymentCardToSpreedly(
+                    spreedlyPaymentCard,
+                    spreedlyEnvironmentKey!!
+                )
+                withContext(Dispatchers.Main) {
+                    try {
+                        val response = spreedlyRequest.await()
+                        card.card.token = response.transaction.payment_method.token
+                        card.card.fingerprint = response.transaction.payment_method.fingerprint
+                        card.card.first_six_digits =
+                            response.transaction.payment_method.first_six_digits
+                        card.card.last_four_digits =
+                            response.transaction.payment_method.last_four_digits
 
-                    val request = apiService.addPaymentCardAsync(card)
-                    withContext(Dispatchers.Main) {
-                        try {
-                            val response = request.await()
-                            paymentCardDao.store(response)
-                            mutableAddCard.value = response
-                        } catch (e: Throwable) {
-                            error.value = e
-                        }
+                        doAddPaymentCard(card, mutableAddCard, error)
+                    } catch (e: Throwable) {
+                        error.value = e
                     }
-                } catch (e: Throwable) {
-                    error.value = e
                 }
             }
+        } else {
+            doAddPaymentCard(card, mutableAddCard, error)
         }
     }
 
@@ -98,6 +103,25 @@ class CardTermsAndConditionsRepository(
                     localMembershipPlans.value = response
                 } catch (e: Throwable) {
                     Log.d(CardTermsAndConditionsRepository::class.simpleName, e.toString())
+                }
+            }
+        }
+    }
+
+    private fun doAddPaymentCard(
+        card: PaymentCardAdd,
+        mutableAddCard: MutableLiveData<PaymentCard>,
+        error: MutableLiveData<Throwable>
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val request = apiService.addPaymentCardAsync(card)
+            withContext(Dispatchers.Main) {
+                try {
+                    val response = request.await()
+                    paymentCardDao.store(response)
+                    mutableAddCard.value = response
+                } catch (e: Throwable) {
+                    error.value = e
                 }
             }
         }
