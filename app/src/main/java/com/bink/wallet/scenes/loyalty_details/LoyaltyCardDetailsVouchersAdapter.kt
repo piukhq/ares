85b7e784d28fc5ade995674ffaf6c374743f897e
Catalin Darjan <cdarjan@bink.com>
2020-03-13 12:56:56 +0200

    Move ViewHolders for Vouchers into separate classes

--- app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsVouchersAdapter.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_details/LoyaltyCardDetailsVouchersAdapter.kt
@@ -7,9 +7,10 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bink.wallet.R
-import com.bink.wallet.StampVoucherBinding
 import com.bink.wallet.databinding.DetailVoucherItemBinding
 import com.bink.wallet.model.response.membership_card.Voucher
+import com.bink.wallet.scenes.loyalty_details.voucherviewholders.AccumulatorVouchersViewHolder
+import com.bink.wallet.scenes.loyalty_details.voucherviewholders.StampVouchersViewHolder
 import com.bink.wallet.utils.*
 import com.bink.wallet.utils.enums.VoucherStates
 import kotlin.math.roundToInt
@@ -43,7 +44,7 @@ class LoyaltyCardDetailsVouchersAdapter(
                     parent,
                     false
                 ) as DetailVoucherItemBinding
-                VouchersViewHolder(binding, onClickListener)
+                AccumulatorVouchersViewHolder(binding, onClickListener)
             }
         }
     }
@@ -53,7 +54,7 @@ class LoyaltyCardDetailsVouchersAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (getItemViewType(position)) {
             STAMP_VOUCHER -> (holder as StampVouchersViewHolder).bind(vouchers[position])
-            else -> (holder as VouchersViewHolder).bind(vouchers[position])
+            else -> (holder as AccumulatorVouchersViewHolder).bind(vouchers[position])
         }
     }
 
@@ -61,183 +62,6 @@ class LoyaltyCardDetailsVouchersAdapter(
         return when (vouchers[position].earn?.type) {
             VOUCHER_TYPE_STAMPS -> STAMP_VOUCHER
             else -> PROGRESS_VOUCHER
-        }
-    }
-
-    class StampVouchersViewHolder(
-        var binding: StampVoucherBinding,
-        val onClickListener: (Any) -> Unit = {}
-    ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(voucher: Voucher) {
-            binding.root.setOnClickListener {
-                onClickListener(voucher)
-            }
-            binding.voucher = voucher
-
-            if (voucher.state == VoucherStates.REDEEMED.state ||
-                voucher.state == VoucherStates.EXPIRED.state ||
-                voucher.state == VoucherStates.NONE.state
-            ) {
-                voucher.expiry_date?.let {
-                    binding.collectedTitle.visibility = View.GONE
-                    binding.collectedAmount.visibility = View.GONE
-                    displayDate(it)
-                }
-            }
-        }
-
-        private fun displayDate(date: Long) {
-            with(binding.voucherDate) {
-                visibility = View.VISIBLE
-                setTimestamp(date, binding.root.context.getString(R.string.voucher_entry_date))
-            }
-        }
-    }
-
-    class VouchersViewHolder(
-        var binding: DetailVoucherItemBinding,
-        val onClickListener: (Any) -> Unit = {}
-    ) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(thisVoucher: Voucher) {
-            with(binding) {
-                voucher = thisVoucher
-                thisVoucher.burn?.let {
-                    if (it.value != null) {
-                        title.text = ValueDisplayUtils.displayValue(
-                            it.value,
-                            it.prefix,
-                            it.suffix,
-                            it.currency,
-                            it.type
-                        )
-                    }
-                }
-                root.apply {
-                    this.setOnClickListener {
-                        onClickListener(thisVoucher)
-                    }
-                }
-            }
-            binding.executePendingBindings()
-            when (thisVoucher.state) {
-                VoucherStates.IN_PROGRESS.state,
-                VoucherStates.ISSUED.state -> {
-                    thisVoucher.earn?.let { earn ->
-                        if (earn.target_value != null) {
-                            if (earn.value != null) {
-                                binding.spentAmount.text = ValueDisplayUtils.displayValue(
-                                    earn.value,
-                                    earn.prefix,
-                                    earn.suffix,
-                                    earn.currency,
-                                    forceTwoDecimals = true
-                                )
-                            }
-                            displayForEarning(thisVoucher)
-                            with(binding) {
-                                progressBar.max =
-                                    (earn.target_value * FLOAT_ONE_HUNDRED).roundToInt()
-                                progressBar.progress =
-                                    ((earn.value ?: FLOAT_ZERO) * FLOAT_ONE_HUNDRED).roundToInt()
-                            }
-                        } else {
-                            hideEarnBurnValues()
-                        }
-                    } ?: run {
-                        hideEarnBurnValues()
-                    }
-                    if (thisVoucher.state == VoucherStates.ISSUED.state) {
-                        fillProgressBar()
-                    }
-                }
-                VoucherStates.EXPIRED.state -> {
-                    thisVoucher.expiry_date?.let {
-                        displayDate(it)
-                    }
-                    displayForEarning(thisVoucher)
-                    hideEarnBurnValues()
-                    fillProgressBar()
-                }
-                VoucherStates.REDEEMED.state -> {
-                    thisVoucher.date_redeemed?.let {
-                        displayDate(it)
-                    }
-                    displayForEarning(thisVoucher)
-                    hideEarnBurnValues()
-                    fillProgressBar()
-                }
-                else -> {
-                    hideEarnBurnValues()
-                    fillProgressBar()
-                }
-            }
-            setProgressDrawable(thisVoucher.state)
-        }
-
-        private fun displayForEarning(thisVoucher: Voucher) {
-            thisVoucher.earn?.let { earn ->
-                binding.apply {
-                    subtitle.text = thisVoucher.subtext.plus(SPACE).plus(
-                        ValueDisplayUtils.displayValue(
-                            earn.target_value,
-                            earn.prefix,
-                            earn.suffix,
-                            earn.currency
-                        )
-                    )
-                    goalAmount.text =
-                        ValueDisplayUtils.displayValue(
-                            earn.target_value,
-                            earn.prefix,
-                            earn.suffix,
-                            earn.currency,
-                            forceTwoDecimals = true
-                        )
-                }
-            }
-        }
-
-        private fun displayDate(date: Long) {
-            with(binding.voucherDate) {
-                visibility = View.VISIBLE
-                setTimestamp(date, binding.root.context.getString(R.string.voucher_entry_date))
-            }
-        }
-
-        private fun hideEarnBurnValues() {
-            with(binding) {
-                goalTitle.visibility = View.GONE
-                goalAmount.visibility = View.GONE
-                spentTitle.visibility = View.GONE
-                spentAmount.visibility = View.GONE
-            }
-        }
-
-        private fun fillProgressBar() {
-            with(binding) {
-                progressBar.max = INT_ONE_HUNDRED
-                progressBar.progress = INT_ONE_HUNDRED
-            }
-        }
-
-        private fun setProgressDrawable(state: String?) {
-            with(binding.progressBar) {
-                progressDrawable =
-                    ContextCompat.getDrawable(
-                        context,
-                        when (state) {
-                            VoucherStates.ISSUED.state ->
-                                R.drawable.loyalty_voucher_progress_earned
-                            VoucherStates.REDEEMED.state ->
-                                R.drawable.loyalty_voucher_progress_redeemed
-                            VoucherStates.EXPIRED.state ->
-                                R.drawable.loyalty_voucher_progress_expired
-                            else ->
-                                R.drawable.loyalty_voucher_progress_bar
-                        }
-                    )
-            }
         }
     }
 
---
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_details/voucherviewholders/StampVouchersViewHolder.kt
@@ -0,0 +1,36 @@
+package com.bink.wallet.scenes.loyalty_details.voucherviewholders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bink.wallet.R
+import com.bink.wallet.StampVoucherBinding
+import com.bink.wallet.model.response.membership_card.Voucher
+import com.bink.wallet.utils.enums.VoucherStates
+import com.bink.wallet.utils.setTimestamp
+
+class StampVouchersViewHolder(
+    var binding: StampVoucherBinding,
+    val onClickListener: (Any) -> Unit = {}
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(voucher: Voucher) {
+        binding.voucher = voucher
+
+        if (voucher.state == VoucherStates.REDEEMED.state ||
+            voucher.state == VoucherStates.EXPIRED.state ||
+            voucher.state == VoucherStates.NONE.state
+        ) {
+            voucher.expiry_date?.let {
+                binding.collectedTitle.visibility = View.GONE
+                binding.collectedAmount.visibility = View.GONE
+                displayDate(it)
+            }
+        }
+    }
+
+    private fun displayDate(date: Long) {
+        with(binding.voucherDate) {
+            visibility = View.VISIBLE
+            setTimestamp(date, binding.root.context.getString(R.string.voucher_entry_date))
+        }
+    }
+}
