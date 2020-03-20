521f9eccb8be091a13ba2a78cb1ecbb54a0e8ce5
Catalin Darjan <cdarjan@bink.com>
2020-03-20 12:34:39 +0200

    AB20-640 Swap Content-Type and Accept values   Fix Accept header value

--- app/src/main/java/com/bink/wallet/di/NetworkModule.kt
+++ app/src/main/java/com/bink/wallet/di/NetworkModule.kt
@@ -54,8 +54,8 @@ fun provideDefaultOkHttpClient(appContext: Context): OkHttpClient {
         }
         val request = chain.request().url().newBuilder().build()
         val newRequest = chain.request().newBuilder()
-            .header("Content-Type", "application/json;v1.1")
-            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json; v=1.1")
             .header("Authorization", jwtToken ?: EMPTY_STRING)
             .url(request)
             .build()
