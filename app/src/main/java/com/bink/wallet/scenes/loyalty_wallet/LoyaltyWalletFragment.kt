eabe8a9554a03498bbfbfb55e4d3c3b965cf5952
Enoch Hankombo <enochhankombo@c02cme4rmd6m.home>
2020-06-22 12:55:44 +0100

    Change wildcard import

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletFragment.kt
@@ -19,8 +19,15 @@ import com.bink.wallet.model.response.membership_card.UserDataResult
 import com.bink.wallet.model.response.membership_plan.MembershipPlan
 import com.bink.wallet.scenes.loyalty_wallet.RecyclerItemTouchHelper.RecyclerItemTouchHelperListener
 import com.bink.wallet.scenes.wallets.WalletsFragmentDirections
-import com.bink.wallet.utils.*
+import com.bink.wallet.utils.ApiErrorUtils
 import com.bink.wallet.utils.FirebaseEvents.LOYALTY_WALLET_VIEW
+import com.bink.wallet.utils.UtilFunctions
+import com.bink.wallet.utils.ZendeskUtils
+import com.bink.wallet.utils.displayModalPopup
+import com.bink.wallet.utils.logDebug
+import com.bink.wallet.utils.navigateIfAdded
+import com.bink.wallet.utils.observeErrorNonNull
+import com.bink.wallet.utils.observeNonNull
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
