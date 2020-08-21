423a27bea7fba30497696b61e567c22ded677983
Enoch Hankombo <ehankombo@bink.com>
2020-08-21 15:40:04 +0100

    Removed voucher state check

--- app/src/main/java/com/bink/wallet/utils/ValueDisplayTargetUtils.kt
+++ app/src/main/java/com/bink/wallet/utils/ValueDisplayTargetUtils.kt
@@ -13,51 +13,25 @@ fun Context.displayVoucherEarnAndTarget(voucher: Voucher): String {
             val earn = voucher.earn
             val burn = voucher.burn
             earn.value?.let { earn_value ->
-                return when (voucher.state) {
-                    VoucherStates.IN_PROGRESS.state -> setInProgressVoucherValues(this, earn)
-                    else ->
-                        getString(
-                            R.string.loyalty_wallet_plr_value,
-                            ValueDisplayUtils.displayValue(
-                                earn_value,
-                                earn.prefix,
-                                earn.suffix,
-                                earn.currency
-                            ),
-                            ValueDisplayUtils.displayValue(
-                                target_value,
-                                burn?.prefix,
-                                burn?.suffix,
-                                burn?.currency
-                            )
-                        )
+                return  getString(
+                    R.string.loyalty_wallet_plr_value,
+                    ValueDisplayUtils.displayValue(
+                        earn.value,
+                        earn.prefix,
+                        null,
+                        earn.currency
+                    ),
+                    ValueDisplayUtils.displayValue(
+                        earn.target_value,
+                        earn.prefix,
+                        earn.suffix,
+                        earn.currency
+                    )
+                )
                 }
 
             }
         }
-    }
     return EMPTY_STRING
 }
 
-fun setInProgressVoucherValues(
-    context: Context,
-    earn: Earn
-): String {
-
-    return context.getString(
-        R.string.loyalty_wallet_plr_value,
-        ValueDisplayUtils.displayValue(
-            earn.value,
-            earn.prefix,
-            null,
-            earn.currency
-        ),
-        ValueDisplayUtils.displayValue(
-            earn.target_value,
-            earn.prefix,
-            earn.suffix,
-            earn.currency
-        )
-    )
-
-}
