38fcc319169418bfef91207b858290baff536857
Enoch Hankombo <ehankombo@bink.com>
2020-08-25 14:58:14 +0000

    Revert "Merge branch..."
    This reverts merge request !540

--- app/src/main/java/com/bink/wallet/utils/ValueDisplayTargetUtils.kt
+++ app/src/main/java/com/bink/wallet/utils/ValueDisplayTargetUtils.kt
@@ -2,35 +2,31 @@ package com.bink.wallet.utils
 
 import android.content.Context
 import com.bink.wallet.R
-import com.bink.wallet.model.response.membership_card.Burn
-import com.bink.wallet.model.response.membership_card.Earn
 import com.bink.wallet.model.response.membership_card.Voucher
-import com.bink.wallet.utils.enums.VoucherStates
 
 fun Context.displayVoucherEarnAndTarget(voucher: Voucher): String {
     voucher.earn?.target_value?.let { target_value ->
         if (target_value != FLOAT_ZERO) {
             val earn = voucher.earn
+            val burn = voucher.burn
             earn.value?.let { earn_value ->
-                return  getString(
+                return getString(
                     R.string.loyalty_wallet_plr_value,
                     ValueDisplayUtils.displayValue(
                         earn_value,
                         earn.prefix,
-                        null,
+                        earn.suffix,
                         earn.currency
                     ),
                     ValueDisplayUtils.displayValue(
                         target_value,
-                        earn.prefix,
-                        earn.suffix,
-                        earn.currency
+                        burn?.prefix,
+                        burn?.suffix,
+                        burn?.currency
                     )
                 )
-                }
-
             }
         }
+    }
     return EMPTY_STRING
 }
-
