ec2b46b606c39fedaa6e4ffce3c0020dc26cb13a
Joshua Best <Jbest@bink.com>
2020-10-28 15:39:41 +0000

    Added logo and names

--- app/src/main/java/com/bink/wallet/scenes/who_we_are/WhoWeAreFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/who_we_are/WhoWeAreFragment.kt
@@ -22,6 +22,20 @@ class WhoWeAreFragment : BaseFragment<WhoWeAreViewModel, WhoWeAreFragmentBinding
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        binding.nameList.adapter = WhoWeAreAdapter(ArrayList<String>().also {
+            it.add("Paul Batty")
+            it.add("Enoch Hankombo")
+            it.add("Joshua Best")
+            it.add("Susanne King")
+            it.add("Srikalyani Kotha")
+            it.add("Marius Lobontiu")
+            it.add("Connor McFadden")
+            it.add("Carmen Muntean")
+            it.add("Teodora Popescu")
+            it.add("Mara Savan")
+            it.add("Karl Sigiscar")
+            it.add("Max Woodhams")
+        })
     }
 
     override fun builder(): FragmentToolbar {
