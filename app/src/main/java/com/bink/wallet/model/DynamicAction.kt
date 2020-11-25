c9b3610d38006644c215802668d0706e19bbf791
Joshua Best <Jbest@bink.com>
2020-11-25 15:05:19 +0000

    Improved Dynamic Action object

--- app/src/main/java/com/bink/wallet/model/DynamicAction.kt
+++ app/src/main/java/com/bink/wallet/model/DynamicAction.kt
@@ -1,11 +1,69 @@
 package com.bink.wallet.model
 
-data class DynamicAction(val name: String, val type: String, val startDate: Int, val endDate: Int, val arrayList: ArrayList<DynamicActionLocation>, val event: DynamicActionEvent)
+import com.google.gson.annotations.SerializedName
 
-data class DynamicActionLocation(val icon: String, val screen: String, val area: String, val action: String)
+data class DynamicAction(
+    val name: String?,
+    val type: DynamicActionType?,
+    val start_date: Int?,
+    val end_date: Int?,
+    val locations: ArrayList<DynamicActionLocation>?,
+    val event: DynamicActionEvent?
+)
 
-data class DynamicActionEvent(val type: String, val body: DynamicActionEventBody)
+enum class DynamicActionType {
+    @SerializedName("xmas")
+    XMAS
+}
 
-data class DynamicActionEventBody(val title: String, val description: String, val cta: DynamicActionEventBodyCta)
+data class DynamicActionLocation(
+    val icon: String?,
+    val screen: DynamicActionScreen?,
+    val area: DynamicActionArea?,
+    val action: DynamicActionHandler?
+)
 
-data class DynamicActionEventBodyCta(val title: String, val action: String)
+enum class DynamicActionScreen {
+    @SerializedName("loyalty_wallet")
+    LOYALTY_WALLET,
+
+    @SerializedName("payment_wallet")
+    PAYMENT_WALLET
+
+}
+
+enum class DynamicActionArea {
+    @SerializedName("left_top_bar")
+    LEFT_TOP_BAR
+}
+
+enum class DynamicActionHandler {
+    @SerializedName("single_tap")
+    SINGLE_TAP
+}
+
+data class DynamicActionEvent(
+    val type: DynamicActionEventType?,
+    val body: DynamicActionEventBody?
+)
+
+enum class DynamicActionEventType {
+    @SerializedName("launch_modal")
+    LAUNCH_MODAL
+}
+
+data class DynamicActionEventBody(
+    val title: String?,
+    val description: String?,
+    val cta: DynamicActionEventBodyCta?
+)
+
+data class DynamicActionEventBodyCta(
+    val title: String?,
+    val action: DynamicActionEventBodyCTAHandler?
+)
+
+enum class DynamicActionEventBodyCTAHandler {
+    @SerializedName("zd_contact_us")
+    ZENDESK_CONTACT_US
+}
