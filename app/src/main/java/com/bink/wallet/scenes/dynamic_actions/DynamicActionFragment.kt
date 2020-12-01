9b0fdad854e04796e00a9154c60db7df5956df95
Joshua Best <Jbest@bink.com>
2020-12-01 11:26:44 +0000

    Creating snow effect

--- app/src/main/java/com/bink/wallet/scenes/dynamic_actions/DynamicActionFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/dynamic_actions/DynamicActionFragment.kt
@@ -1,19 +1,28 @@
 package com.bink.wallet.scenes.dynamic_actions
 
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.DynamicActionFragmentBinding
 import com.bink.wallet.model.DynamicActionEventBodyCTAHandler
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class DynamicActionFragment : BaseFragment<DynamicActionViewModel, DynamicActionFragmentBinding>() {
 
     override val viewModel: DynamicActionViewModel by viewModel()
 
     override val layoutRes = R.layout.dynamic_action_fragment
+
+    private var snowList: ArrayList<Snow> = ArrayList()
+    private lateinit var runnable: Runnable
 
     override fun builder(): FragmentToolbar {
         return FragmentToolbar.Builder()
@@ -40,9 +49,32 @@ class DynamicActionFragment : BaseFragment<DynamicActionViewModel, DynamicAction
                             }
                         }
                     }
+
+
                 }
             }
         }
+
+        setUpSnowEffect()
+    }
+
+    private fun setUpSnowEffect() {
+        val point = Point()
+        activity?.windowManager?.defaultDisplay?.getRealSize(point)
+
+        val container: ViewGroup = activity?.window?.decorView as ViewGroup
+        val bgHandler = Handler()
+
+        for (i in 0 until 200) {
+            snowList.add(Snow(requireContext(), point.x.toFloat(), point.y.toFloat(), container))
+        }
+
+        runnable = Runnable {
+            for (snow: Snow in snowList)
+                snow.update()
+            bgHandler.postDelayed(runnable, 10)
+        }
+        bgHandler.post(runnable)
     }
 
     private fun launchDynamicActionEventCta(action: DynamicActionEventBodyCTAHandler){
@@ -53,4 +85,47 @@ class DynamicActionFragment : BaseFragment<DynamicActionViewModel, DynamicAction
         }
     }
 
+    override fun onPause() {
+        var container: ViewGroup = activity?.window?.decorView as ViewGroup
+        if(snowList.isNotEmpty()){
+            for(snowflake in snowList) {
+                container.removeView(snowflake.snowflake)
+            }
+        }
+        super.onPause()
+    }
+
+}
+
+class Snow(context: Context, private val screenW: Float, private val screenH: Float, parent: ViewGroup) {
+    var snowflake: ImageView = ImageView(context)
+    private var distance = Random().nextFloat() * 0.5f + 0.5f
+    private val fallingSpeed = 6
+    private val windSpeed = 4
+
+    init {
+        snowflake.setBackgroundResource(R.mipmap.ic_snowflake_foreground)
+        parent.addView(snowflake)
+
+        val dimens = (Random().nextFloat() * screenH * 0.015 / distance).toInt()
+        snowflake.layoutParams.height = dimens
+        snowflake.layoutParams.width = dimens
+        snowflake.alpha = (1.0f - distance * 0.7).toFloat()
+        snowflake.translationX = Random().nextFloat() * screenW
+        snowflake.translationY = Random().nextFloat() * screenH
+        snowflake.rotation = Random().nextFloat() * 360
+    }
+
+    fun update() {
+        // far=slow, close=fast
+        snowflake.translationY = (snowflake.translationY + fallingSpeed * (1 - distance * 0.8)).toFloat()
+        snowflake.translationX = (snowflake.translationX + windSpeed * (1 - distance * 0.7)).toFloat()
+
+        if (snowflake.translationY > screenH)
+            snowflake.translationY = snowflake.translationY - screenH
+        if (snowflake.translationX > screenW)
+            snowflake.translationX = snowflake.translationX - screenW
+
+        snowflake.rotation = snowflake.rotation +  (Random().nextFloat() * 0.6f - 0.3f) * 5
+    }
 }
