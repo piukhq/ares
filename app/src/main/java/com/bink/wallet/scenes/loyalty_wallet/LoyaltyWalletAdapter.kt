84cd16dc87a5a4f058aecaacb90e5648673bb9e9
Joshua Best <Jbest@bink.com>
2020-12-09 17:18:14 +0000

    Improved syntax

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletAdapter.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletAdapter.kt
@@ -25,9 +25,6 @@ import com.bink.wallet.utils.displayVoucherEarnAndTarget
 import com.bink.wallet.utils.enums.MembershipCardStatus
 import com.bink.wallet.utils.enums.VoucherStates
 import com.bink.wallet.utils.formatBalance
-import java.text.NumberFormat
-import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.properties.Delegates
 
 class LoyaltyWalletAdapter(
@@ -232,22 +229,13 @@ class LoyaltyWalletAdapter(
                             }
                     } else if (!item.balances.isNullOrEmpty()) {
                         item.balances?.firstOrNull()?.let { balance ->
-                            when (balance?.prefix != null) {
+                            when (balance.prefix != null) {
                                 true -> {
-                                    loyaltyValue.text = when (balance?.prefix) {
-                                        "£" -> {
-                                            balance.formatBalance()
-                                        }
-                                        else -> {
-                                            balance?.prefix?.plus(balance.value)
-                                        }
-                                    }
+                                    loyaltyValue.text = balance.formatBalance()
                                 }
-
-
                                 else -> {
-                                    loyaltyValue.text = balance?.value
-                                    loyaltyValueExtra.text = balance?.suffix
+                                    loyaltyValue.text = balance.value
+                                    loyaltyValueExtra.text = balance.suffix
                                 }
                             }
                         }
