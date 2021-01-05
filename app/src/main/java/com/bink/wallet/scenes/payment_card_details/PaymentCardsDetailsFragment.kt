b0701084fba63e27fabd7ec32c7277b68e001003
Joshua Best <Jbest@bink.com>
2021-01-05 12:55:13 +0000

    Displaying loyalty cards in PCD in order

--- app/src/main/java/com/bink/wallet/scenes/payment_card_details/PaymentCardsDetailsFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/payment_card_details/PaymentCardsDetailsFragment.kt
@@ -17,21 +17,11 @@ import com.bink.wallet.model.auth.User
 import com.bink.wallet.model.response.membership_card.MembershipCard
 import com.bink.wallet.model.response.membership_plan.MembershipPlan
 import com.bink.wallet.model.response.payment_card.PaymentCard
-import com.bink.wallet.utils.EMPTY_STRING
+import com.bink.wallet.utils.*
 import com.bink.wallet.utils.FirebaseEvents.PAYMENT_DETAIL_VIEW
-import com.bink.wallet.utils.PENDING_CARD
-import com.bink.wallet.utils.PLAN_ALREADY_EXISTS
-import com.bink.wallet.utils.PROD_ARTICLE_ID
-import com.bink.wallet.utils.PaymentCardUtils
-import com.bink.wallet.utils.SANDBOX_ARTICLE_ID
-import com.bink.wallet.utils.SCROLL_DELAY
 import com.bink.wallet.utils.UtilFunctions.isNetworkAvailable
 import com.bink.wallet.utils.enums.BuildTypes
 import com.bink.wallet.utils.enums.CardType
-import com.bink.wallet.utils.goToContactUsForm
-import com.bink.wallet.utils.navigateIfAdded
-import com.bink.wallet.utils.observeErrorNonNull
-import com.bink.wallet.utils.observeNonNull
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import kotlinx.coroutines.runBlocking
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -39,6 +29,7 @@ import retrofit2.HttpException
 import zendesk.support.guide.ViewArticleActivity
 import zendesk.support.requestlist.RequestListActivity
 import java.util.*
+import kotlin.collections.ArrayList
 
 class PaymentCardsDetailsFragment :
     BaseFragment<PaymentCardsDetailsViewModel, PaymentCardsDetailsFragmentBinding>() {
@@ -266,7 +257,7 @@ class PaymentCardsDetailsFragment :
                     availablePllAdapter = AvailablePllAdapter(
                         pCard,
                         plans,
-                        pllCards,
+                        WalletOrderingUtil.getSavedLoyaltyCardWallet(pllCards as ArrayList<Any>) as ArrayList<MembershipCard>,
                         onLinkStatusChange = ::onLinkStatusChange,
                         onItemSelected = ::onItemSelected
                     )
