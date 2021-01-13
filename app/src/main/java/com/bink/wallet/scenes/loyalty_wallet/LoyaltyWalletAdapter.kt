274754edf4b660ec7f469b9a679cfd0d5dde8f5b
Joshua Best <Jbest@bink.com>
2021-01-13 16:46:29 +0000

    added in method for swapping items

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletAdapter.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/LoyaltyWalletAdapter.kt
@@ -24,6 +24,9 @@ import com.bink.wallet.utils.bindings.setVoucherCollectedProgress
 import com.bink.wallet.utils.displayVoucherEarnAndTarget
 import com.bink.wallet.utils.enums.MembershipCardStatus
 import com.bink.wallet.utils.enums.VoucherStates
+import com.bink.wallet.utils.formatBalance
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.properties.Delegates
 
 class LoyaltyWalletAdapter(
@@ -149,6 +152,25 @@ class LoyaltyWalletAdapter(
         })
 
         diff.dispatchUpdatesTo(this)
+    }
+
+    fun onItemMove(fromPosition: Int?, toPosition: Int?): Boolean {
+        fromPosition?.let {
+            toPosition?.let {
+                if (fromPosition < toPosition) {
+                    for (i in fromPosition until toPosition) {
+                        Collections.swap(membershipCards, i, i + 1)
+                    }
+                } else {
+                    for (i in fromPosition downTo toPosition + 1) {
+                        Collections.swap(membershipCards, i, i - 1)
+                    }
+                }
+                notifyItemMoved(fromPosition, toPosition)
+                return true
+            }
+        }
+        return false
     }
 
     inner class PaymentCardWalletJoinHolder(val binding: EmptyLoyaltyItemBinding) :
