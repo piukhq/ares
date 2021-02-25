1dc65e60f6eeb6e9b83e865dfa54f435e6a9c3d7
Enoch Hankombo <ehankombo@bink.com>
2021-02-25 15:22:49 +0000

    WebscrapeRepository cleanup

--- app/src/main/java/com/bink/wallet/utils/LocalPointScraping/WebScrapeRepository.kt
+++ app/src/main/java/com/bink/wallet/utils/LocalPointScraping/WebScrapeRepository.kt
@@ -10,40 +10,15 @@ import kotlinx.coroutines.withContext
 class WebScrapeRepository() {
 
     fun getWebScrapeCredentials(callback: (List<WebScrapeCredentials>?) -> Unit) {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-//                val webScrapeCredentials = webScrapeDao.getWebScrapeCredentials()
-                withContext(Dispatchers.Main) {
-                    callback(webScrapeCredentials)
-                }
-            } catch (e: Exception) {
-                callback(null)
-            }
-        }
+
     }
 
     fun storeWebScrapeCredentials(webScrapeCredentials: WebScrapeCredentials) {
-        CoroutineScope(Dispatchers.IO).launch {
-            withContext(Dispatchers.Main) {
-                try {
-//                    webScrapeDao.storeWebScrapeCredentials(webScrapeCredentials)
-                } catch (e: Exception) {
-                    logDebug(WebScrapeRepository::class.simpleName, e.toString())
-                }
-            }
-        }
+
     }
 
     fun deleteWebScrapeCredentials(membershipPlanId: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
-            withContext(Dispatchers.Main) {
-                try {
-                    webScrapeDao.deleteWebScrapeCredentials(membershipPlanId)
-                } catch (e: Exception) {
-                    logDebug(WebScrapeRepository::class.simpleName, e.toString())
-                }
-            }
-        }
+
     }
 
 }
