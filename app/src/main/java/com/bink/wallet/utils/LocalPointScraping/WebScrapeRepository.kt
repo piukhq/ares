c2815abe997b432d44c48173a84a451fca7bea5c
Enoch Hankombo <ehankombo@bink.com>
2021-02-25 15:28:12 +0000

    WebscrapDao now remoed

--- app/src/main/java/com/bink/wallet/utils/LocalPointScraping/WebScrapeRepository.kt
+++ app/src/main/java/com/bink/wallet/utils/LocalPointScraping/WebScrapeRepository.kt
@@ -1,12 +1,5 @@
 package com.bink.wallet.utils.LocalPointScraping
 
-import com.bink.wallet.data.WebScrapeDao
-import com.bink.wallet.utils.logDebug
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
 class WebScrapeRepository() {
 
     fun getWebScrapeCredentials(callback: (List<WebScrapeCredentials>?) -> Unit) {
