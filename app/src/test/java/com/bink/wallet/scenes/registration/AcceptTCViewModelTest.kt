c865afc25497f74c2749edc654886d90e1aa0b24
Joshua Best <Jbest@bink.com>
2021-04-20 16:05:17 +0100

    TC viewmodel test

--- app/src/test/java/com/bink/wallet/scenes/registration/AcceptTCViewModelTest.kt
+++ app/src/test/java/com/bink/wallet/scenes/registration/AcceptTCViewModelTest.kt
@@ -1,11 +1,13 @@
-package com.bink.wallet.scenes.splash
+package com.bink.wallet.scenes.registration
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.bink.wallet.model.auth.User
 import com.bink.wallet.scenes.login.LoginRepository
+import com.bink.wallet.scenes.loyalty_wallet.wallet.LoyaltyWalletRepository
 import com.bink.wallet.scenes.settings.UserRepository
 import com.bink.wallet.testrules.CoroutineTestRule
+import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -15,10 +17,11 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
+import java.lang.RuntimeException
 
 @RunWith(MockitoJUnitRunner::class)
 
-class SplashViewModelTest {
+class AcceptTCViewModelTest {
 
     @Rule
     @JvmField
@@ -32,30 +35,49 @@ class SplashViewModelTest {
     private lateinit var loginRepository: LoginRepository
 
     @Mock
+    private lateinit var loyaltyWalletRepository: LoyaltyWalletRepository
+
+    @Mock
     private lateinit var userRepository: UserRepository
 
     @Mock
     private lateinit var userResponseObserver: Observer<User>
 
-    private lateinit var splashViewModel: SplashViewModel
+    @Mock
+    private lateinit var userReturnedObserver: Observer<Boolean>
+
+    private lateinit var acceptTCViewModel: AcceptTCViewModel
 
     private val user = User("Hello", "World", "123")
 
     @Before
     fun setup() {
-        splashViewModel = SplashViewModel(
+        acceptTCViewModel = AcceptTCViewModel(
             loginRepository,
+            loyaltyWalletRepository,
             userRepository
         )
 
-        splashViewModel.getUserResponse.observeForever(userResponseObserver)
+        acceptTCViewModel.getUserResponse.observeForever(userResponseObserver)
+        acceptTCViewModel.userReturned.observeForever(userReturnedObserver)
     }
 
     @Test
     fun `Get user success`() = runBlocking {
         Mockito.`when`(userRepository.getUserDetails()).thenReturn(user)
-        splashViewModel.getCurrentUser()
+        acceptTCViewModel.getCurrentUser()
         verify(userResponseObserver).onChanged(user)
+    }
+
+    @Test
+    fun `Get user failure`() = runBlocking {
+//        val error = java.lang.Exception()
+////        given(userRepository.getUserDetails()).willAnswer {
+////            throw error
+////        }
+//        Mockito.`when`(userRepository.getUserDetails()).thenThrow(error)
+//        acceptTCViewModel.getCurrentUser()
+//        verify(userReturnedObserver).onChanged(false)
     }
 
 }
