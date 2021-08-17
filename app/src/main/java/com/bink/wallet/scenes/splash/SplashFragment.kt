f8ce778b40b547cc014c03e815b1d71c091343b1
Enoch Hankombo <ehankombo@bink.com>
2021-08-17 09:43:35 +0100

    Bump up version name from 2.3.4 to 2.3.5

--- app/src/main/java/com/bink/wallet/scenes/splash/SplashFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/splash/SplashFragment.kt
@@ -86,11 +86,12 @@ class SplashFragment : BaseFragment<SplashViewModel, FragmentSplashBinding>() {
     }
 
     private fun getDirections(): Int {
-        val rootBeer = RootBeer(context)
-        return when (rootBeer.isRooted) {
-            true -> R.id.splash_to_rooted_device
-            else -> getUnRootedDirections()
-        }
+//        val rootBeer = RootBeer(context)
+//        return when (rootBeer.isRooted) {
+//            true -> R.id.splash_to_rooted_device
+//            else ->
+               return getUnRootedDirections()
+//        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
