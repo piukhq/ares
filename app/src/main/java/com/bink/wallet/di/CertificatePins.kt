05ba95984f6323cdbe20595cbfb08057b568a946
Enoch Hankombo <ehankombo@bink.com>
2022-02-08 18:19:43 +0000

    Added new production certificate

--- app/src/main/java/com/bink/wallet/di/CertificatePins.kt
+++ app/src/main/java/com/bink/wallet/di/CertificatePins.kt
@@ -6,6 +6,7 @@ enum class CertificatePins(val domain: String, val hash: String) {
     // NOTE: The signature is base64, so HAS to end in "=" to be valid (or it's ignored)
 //    DEBUG("api.dev.gb.bink.com", "sha256/35Y7j68+91YIgnJV8bQQfGfqwLP1OPnM6PZAjVw79P4="),
 //    STAGING("api.staging.gb.bink.com", "sha256/z7/qT56fkSkCNT8b4CKoIyCuK7MEFytc63IzAyRgD1g="),
-    PROD("api.bink.com", "sha256/U4So9I4CQcjW67YtUpr8RqD90fWq9ydCikF82W7Vtuc=")
+    PROD("api.bink.com", "sha256/U4So9I4CQcjW67YtUpr8RqD90fWq9ydCikF82W7Vtuc="),
+    PROD_2("api.bink.com", "sha256/4TTLsYF+NPCxZrkXK/+zOuUsYYyPKN+w5KMus7qLgHk=")
 
 }
