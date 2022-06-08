0ffef00f205145e0b3dc7f1e438f926597200ff7
Joshua Best <Jbest@bink.com>
2022-06-08 15:49:16 +0100

    All ui displaying

--- app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/barcode/BarcodeFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/loyalty_wallet/barcode/BarcodeFragment.kt
@@ -1,44 +1,30 @@
 package com.bink.wallet.scenes.loyalty_wallet.barcode
 
 import android.os.Bundle
+import android.view.View
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.BarcodeFragmentBinding
 import com.bink.wallet.model.response.membership_card.MembershipCard
-import com.bink.wallet.utils.BarcodeWrapper
-import com.bink.wallet.utils.MembershipPlanUtils
+import com.bink.wallet.utils.*
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>() {
-
-    private val nunitoSans = FontFamily(
-        Font(R.font.nunito_sans, FontWeight.Normal),
-        Font(R.font.nunito_sans_extrabold, FontWeight.ExtraBold),
-        Font(R.font.nunito_sans_light, FontWeight.Light)
-    )
 
     override fun builder(): FragmentToolbar {
         return FragmentToolbar.Builder()
@@ -50,26 +36,13 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
     override val layoutRes: Int
         get() = R.layout.barcode_fragment
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         arguments?.let {
             BarcodeFragmentArgs.fromBundle(it).apply {
                 viewModel.membershipPlan.value = currentMembershipPlan
                 viewModel.membershipCard.value = membershipCard
-                viewModel.barcode.value = BarcodeWrapper(membershipCard)
-
-                viewModel.isBarcodeAvailable.set(!membershipCard.card?.barcode.isNullOrEmpty())
-                viewModel.isCardNumberAvailable.set(!membershipCard.card?.membership_id.isNullOrEmpty())
-
-                membershipCard.card?.let { card ->
-                    if (viewModel.isCardNumberAvailable.get()) {
-                        viewModel.cardNumber.set(card.membership_id)
-                    }
-                    if (viewModel.isBarcodeAvailable.get()) {
-                        viewModel.barcodeNumber.set(card.barcode)
-                    }
-                }
 
                 binding.title.text = currentMembershipPlan.account?.company_name
                 binding.composeView.setContent {
@@ -83,19 +56,27 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
 
     @Composable
     fun BarcodeScreen(membershipCard: MembershipCard) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxHeight()
                 .fillMaxWidth()
                 .padding(36.dp)
+                .verticalScroll(rememberScrollState())
         ) {
-            //Header(membershipCard)
+            Header(membershipCard)
             if (!membershipCard.card?.membership_id.isNullOrEmpty()) {
                 MembershipNumber(membershipCard.card?.membership_id!!)
             }
-            if (!membershipCard.card?.barcode.isNullOrEmpty()) {
+            if (!membershipCard.card?.barcode.isNullOrEmpty() && membershipCard.card?.barcode != membershipCard.card?.membership_id) {
                 BarcodeNumber(membershipCard.card?.barcode!!)
             }
+            GradientButton(
+                text = "Report Issue",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = {
+
+                }
+            )
         }
     }
 
@@ -105,7 +86,6 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
             MembershipPlanUtils.loadBarcode(requireContext(), BarcodeWrapper(membershipCard))?.let {
                 Image(bitmap = it.asImageBitmap(), contentDescription = "Barcode")
             }
-
             Text(
                 modifier = Modifier.padding(top = 16.dp), text = getString(R.string.barcode_description),
                 fontFamily = nunitoSans,
@@ -117,9 +97,10 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
 
     @Composable
     fun MembershipNumber(text: String) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             Text(
                 modifier = Modifier.padding(top = 16.dp, bottom = 16.dp), text = getString(R.string.membership_number_title),
                 fontFamily = nunitoSans,
@@ -127,60 +108,61 @@ class BarcodeFragment : BaseFragment<BarcodeViewModel, BarcodeFragmentBinding>()
                 fontSize = 21.sp
             )
 
-            NumberRow(text = text.split(""))
+            NumberRow(text = text)
         }
     }
 
     @Composable
     fun BarcodeNumber(text: String) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             Text(
-                modifier = Modifier.padding(top = 16.dp, bottom = 16.dp), text = getString(R.string.membership_number_title),
+                modifier = Modifier.padding(top = 16.dp, bottom = 16.dp), text = getString(R.string.barcode_text),
                 fontFamily = nunitoSans,
                 fontWeight = FontWeight.Bold,
                 fontSize = 21.sp
             )
 
-            NumberRow(text = text.split(""))
+            NumberRow(text = text)
         }
     }
 
     @Composable
-    fun NumberRow(text: List<String>) {
-        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 41.dp)) {
-            itemsIndexed(items = text) { index, char ->
-                val backgroundColour = if (index % 2 == 0) Color.Gray else Color.LightGray
-                Column(modifier = Modifier
+    fun NumberRow(text: String) {
+        val textAsList = text.split("").filter { it != " " && it != "" }
+        GridItems(
+            data = textAsList,
+            columnCount = 7
+        ) { itemData, index ->
+            val backgroundColour = if (index % 2 == 0) Color.Gray else Color.LightGray
+            Column(
+                modifier = Modifier
                     .background(backgroundColour)
-                    .width(41.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Row {
-                        Text(
-                            text = char,
-                            Modifier.padding(top = 10.dp, bottom = 5.dp, start = 5.dp, end = 5.dp),
-                            fontFamily = nunitoSans,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 40.sp
-                        )
-                    }
-                    Column(verticalArrangement = Arrangement.Bottom) {
-                        Text(
-                            text = index.toString(),
-                            fontFamily = nunitoSans,
-                            fontWeight = FontWeight.Light,
-                            fontSize = 8.sp
-                        )
-                    }
+                    .widthIn(min = 46.dp)
+                    .height(86.dp), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row {
+                    Text(
+                        text = itemData,
+                        Modifier.padding(top = 10.dp, bottom = 5.dp, start = 5.dp, end = 5.dp),
+                        fontFamily = nunitoSans,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp
+                    )
+                }
+                Column(verticalArrangement = Arrangement.Bottom) {
+                    Text(
+                        text = (index + 1).toString(),
+                        fontFamily = nunitoSans,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 8.sp
+                    )
                 }
             }
         }
-    }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        MembershipNumber("ABC123!@£")
     }
 
 }
---
+++ app/src/main/java/com/bink/wallet/utils/ComposeComponents.kt
@@ -0,0 +1,86 @@
+package com.bink.wallet.utils
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bink.wallet.R
+
+val nunitoSans = FontFamily(
+    Font(R.font.nunito_sans, FontWeight.Normal),
+    Font(R.font.nunito_sans_extrabold, FontWeight.ExtraBold),
+    Font(R.font.nunito_sans_light, FontWeight.Light)
+)
+
+@Composable
+fun <T> GridItems(
+    data: List<T>,
+    columnCount: Int,
+    modifier: Modifier = Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    itemContent: @Composable BoxScope.(T, Int) -> Unit,
+) {
+    val size = data.count()
+    repeat(data.size) { rowIndex ->
+        Row(
+            horizontalArrangement = horizontalArrangement,
+            modifier = modifier
+        ) {
+            for (columnIndex in 0 until columnCount) {
+                val itemIndex = rowIndex * columnCount + columnIndex
+                if (itemIndex < size) {
+                    Box(
+                        modifier = Modifier.weight(1F, fill = true),
+                        propagateMinConstraints = true
+                    ) {
+                        itemContent(data[itemIndex], itemIndex)
+                    }
+                } else {
+                    Spacer(Modifier.weight(1F, fill = true))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GradientButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { },
+) {
+    Spacer(modifier = Modifier.height(16.dp))
+    Button(
+        modifier = modifier.clip(RoundedCornerShape(25.dp)),
+        contentPadding = PaddingValues(),
+        onClick = { onClick() },
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Brush.horizontalGradient(listOf(Color(0xFF3D908F),Color(0xFF194B53))))
+                .then(modifier),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = text,
+                fontFamily = nunitoSans,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(16.dp)
+                )
+        }
+    }
+}
