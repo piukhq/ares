b9da38c59ada07a5ac537a40fc5d5087fb1f85eb
Joshua Best <Jbest@bink.com>
2022-07-25 14:02:06 +0100

    Fixed missing items

--- app/src/main/java/com/bink/wallet/utils/bindings/TextViewBindingAdapters.kt
+++ app/src/main/java/com/bink/wallet/utils/bindings/TextViewBindingAdapters.kt
@@ -3,7 +3,11 @@ package com.bink.wallet.utils.bindings
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bink.wallet.R
+import com.bink.wallet.model.response.membership_card.Burn
 import com.bink.wallet.model.response.membership_card.Earn
+import com.bink.wallet.model.response.membership_card.Voucher
+import com.bink.wallet.utils.ValueDisplayUtils
+import com.bink.wallet.utils.enums.VoucherStates
 
 const val ACCUMULATOR = "accumulator"
 const val STAMP = "stamps"
@@ -17,4 +21,49 @@ fun TextView.setVoucherCollectedProgress(voucherEarn: Earn) {
         voucherEarn.target_value?.toInt(),
         voucherEarn.suffix
     )
+}
+
+@BindingAdapter("voucherEarnSubtitle", requireAll = true)
+fun TextView.setVoucherSubText(voucherEarn: Earn) {
+    text = context.getString(
+        R.string.voucher_stamp_subtext,
+        voucherEarn.target_value?.toInt(),
+        voucherEarn.suffix
+    )
+}
+
+@BindingAdapter("voucherBurn")
+fun TextView.setVoucherTitle(voucherBurn: Burn?) {
+    text = context.getString(
+        R.string.voucher_stamp_title,
+        ValueDisplayUtils.displayValue(
+            voucherBurn?.value,
+            voucherBurn?.prefix,
+            voucherBurn?.suffix,
+            null
+        )
+    )
+}
+
+@BindingAdapter("voucherHeadline")
+fun TextView.setVoucherHeadline(voucher: Voucher) {
+    voucher.state?.let { state ->
+        text = when (state) {
+            VoucherStates.REDEEMED.state,
+            VoucherStates.EXPIRED.state,
+            VoucherStates.CANCELLED.state
+            -> state.capitalize()
+            VoucherStates.ISSUED.state -> context.getString(R.string.earned).capitalize()
+            else ->
+                if (voucher.earn?.type == STAMP)
+                    context.getString(
+                        R.string.voucher_stamp_in_progress_headline,
+                        ValueDisplayUtils.displayFormattedHeadline(voucher.earn)
+                    ) else context.getString(
+                    R.string.voucher_accumulator_in_progress_headline,
+                    ValueDisplayUtils.displayFormattedHeadline(voucher.earn)
+                )
+        }
+    }
+
 }
