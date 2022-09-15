cc05904f60d8c1af05103d3dc54ee0aae776116e
Joshua Best <Jbest@bink.com>
2022-09-15 10:15:00 +0100

    Added map clustering

--- app/src/main/java/com/bink/wallet/model/tescolocations/Feature.kt
+++ app/src/main/java/com/bink/wallet/model/tescolocations/Feature.kt
@@ -1,6 +1,8 @@
 package com.bink.wallet.model.tescolocations
 
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -11,5 +13,17 @@ data class Feature(
     @Json(name = "properties")
     val properties: Properties?,
     @Json(name = "type")
-    val type: String?
-)
+    val type: String?,
+) : ClusterItem {
+    override fun getPosition(): LatLng {
+        return LatLng((geometry?.coordinates?.get(1) ?: 0.0), (geometry?.coordinates?.get(0) ?: 0.0))
+    }
+
+    override fun getTitle(): String {
+        return ""
+    }
+
+    override fun getSnippet(): String {
+        return ""
+    }
+}
