7ac8fe3416df9ede15c8f0345bcf1cb9e9e69acc
Joshua Best <Jbest@bink.com>
2022-09-20 15:22:51 +0100

    Creating file for text

--- app/src/main/java/com/bink/wallet/scenes/settings/DebugMenuFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/settings/DebugMenuFragment.kt
@@ -3,13 +3,16 @@ package com.bink.wallet.scenes.settings
 import android.app.AlertDialog
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bink.wallet.BaseFragment
+import com.bink.wallet.BuildConfig
 import com.bink.wallet.R
 import com.bink.wallet.data.SharedPreferenceManager
 import com.bink.wallet.databinding.DebugMenuEditTextBinding
@@ -23,6 +26,7 @@ import com.bink.wallet.utils.enums.BackendVersion
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import com.bumptech.glide.Glide
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
 
 
 class DebugMenuFragment : BaseFragment<DebugMenuViewModel, FragmentDebugMenuBinding>() {
@@ -222,16 +226,27 @@ class DebugMenuFragment : BaseFragment<DebugMenuViewModel, FragmentDebugMenuBind
     }
 
     private fun exportNetworkRequests(content: String?) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.export_network))
-        intent.putExtra(Intent.EXTRA_TEXT, content)
-        startActivity(
-            Intent.createChooser(
-                intent,
-                getString(R.string.export_network)
-            )
+        requireContext().openFileOutput("networkRequests.txt", Context.MODE_PRIVATE).use {
+            it.write(content?.toByteArray())
+        }
+
+        val file = File(requireContext().filesDir, "networkRequests.txt")
+
+        val uri = FileProvider.getUriForFile(
+            requireContext(),
+            BuildConfig.APPLICATION_ID + ".provider",
+            file
         )
+
+        val intent = Intent(Intent.ACTION_SEND)
+
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+
+        intent.type = "*/*"
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+        startActivity(intent)
     }
 
     private fun applyChanges() {
