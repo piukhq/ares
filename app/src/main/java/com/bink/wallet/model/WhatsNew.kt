b4355397f261bb5a5b847ef6408b267ada91ddfc
Joshua Best <Jbest@bink.com>
2023-04-24 12:05:13 +0100

    Basic layout for whats new in place

--- app/src/main/java/com/bink/wallet/model/WhatsNew.kt
+++ app/src/main/java/com/bink/wallet/model/WhatsNew.kt
@@ -8,13 +8,24 @@ data class WhatsNew(
     val id: String? = null,
     val published: Boolean? = null,
     val showFrom: Int? = null,
-    val adhocMessages: List<NewFeature>? = null,
+    val adhocMessages: List<AdHocMessage>? = null,
     val features: List<NewFeature>? = null,
     val merchants: List<NewMerchant>? = null,
 ) : Parcelable
 
+fun WhatsNew.asAnyList(): ArrayList<Any> {
+    val list = arrayListOf<Any>()
+    merchants?.let { list.addAll(it) }
+    features?.let { list.addAll(it) }
+    adhocMessages?.let { list.addAll(it) }
+    return list
+}
+
 @Parcelize
-data class NewFeature(val description: String? = null, val imageUrl: String? = null) : Parcelable
+data class NewFeature(val description: String? = null, val imageUrl: String? = null, val title: String? = null) : Parcelable
+
+@Parcelize
+data class AdHocMessage(val description: String? = null, val imageUrl: String? = null, val title: String? = null) : Parcelable
 
 @Parcelize
 data class NewMerchant(val description: String? = null, val membershipPlanId: String? = null) : Parcelable
--- app/src/main/java/com/bink/wallet/scenes/whats_new/WhatsNewFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/whats_new/WhatsNewFragment.kt
@@ -2,15 +2,41 @@ package com.bink.wallet.scenes.whats_new
 
 import android.os.Bundle
 import android.view.View
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bink.wallet.BaseFragment
 import com.bink.wallet.R
 import com.bink.wallet.databinding.WhatsNewFragmentBinding
+import com.bink.wallet.model.AdHocMessage
+import com.bink.wallet.model.NewFeature
+import com.bink.wallet.model.NewMerchant
+import com.bink.wallet.model.asAnyList
 import com.bink.wallet.theme.AppTheme
+import com.bink.wallet.theme.White
+import com.bink.wallet.utils.ImageViaUrl
+import com.bink.wallet.utils.nunitoSans
 import com.bink.wallet.utils.toolbar.FragmentToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -36,17 +62,228 @@ class WhatsNewFragment :
 
         binding.composeView.setContent {
             AppTheme(viewModel.theme.value) {
-                WhatsNew()
+                WhatsNew(viewModel.whatsNew.value?.asAnyList())
+            }
+        }
+    }
+
+
+    @Preview
+    @Composable
+    private fun Preview() {
+        val whatsNew = arrayListOf<Any>(
+            NewMerchant("Hello", "0"),
+            NewFeature("You can now add a new profile picture!", "https://firebasestorage.googleapis.com/v0/b/bink-ac226.appspot.com/o/new-features%2F0.jpeg?alt=media&token=0e425696-53ab-44a7-b172-3011d7d33094", "New pic!!!"),
+            AdHocMessage("Hello", "", "Title"),
+        )
+        WhatsNew(whatsNew)
+    }
+
+    @Composable
+    private fun WhatsNew(whatsNew: ArrayList<Any>?) {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_padding_size_medium))) {
+            Text(modifier = Modifier.fillMaxWidth(),
+                text = "Whats new",
+                textAlign = TextAlign.Center,
+                color = colorResource(id = R.color.blue_accent),
+                fontSize = 30.sp,
+                fontFamily = nunitoSans,
+                fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_large)))
+            whatsNew?.let {
+                NewList(list = it)
             }
         }
     }
 
     @Composable
-    private fun WhatsNew() {
-        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_padding_size_medium))) {
+    private fun NewList(list: ArrayList<Any>) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.margin_padding_size_large)),
+        ) {
+            items(items = list) { newItem ->
 
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                    shape = RoundedCornerShape(12.dp)) {
+                    when (newItem) {
+                        is NewMerchant -> {
+                            NewMerchant(newMerchant = newItem)
+                        }
+                        is NewFeature -> {
+                            NewFeature(newFeature = newItem)
+                        }
+                        is AdHocMessage -> {
+                            AdHocMessage(adHocMessage = newItem)
+                        }
+                    }
+                }
+            }
         }
     }
 
+    @Composable
+    private fun NewMerchant(newMerchant: NewMerchant) {
+        BoxWithConstraints(modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .background(Color.White)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(500.dp)
+                    .offset(
+                        y = (-80).dp,
+                        x = 110.dp,
+                    )
+                    .rotate(-46f)
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(Color.Red)
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(500.dp)
+                    .offset(
+                        y = (-40).dp,
+                        x = 140.dp,
+                    )
+                    .rotate(-23f)
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(Color.Blue)
+            )
+
+            Column(modifier = Modifier
+                .align(Alignment.Center)
+                .padding(start = 64.dp)) {
+                Text(
+                    text = "Merchant",
+                    fontFamily = nunitoSans,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = "${newMerchant.description}",
+                    fontFamily = nunitoSans,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            }
+
+            Column(modifier = Modifier
+                .align(Alignment.TopCenter)
+                .matchParentSize()
+                .fillMaxHeight()) {
+                FeatureTitle(title = "New Merchant")
+            }
+
+            Image(
+                imageVector = Icons.Filled.ArrowForwardIos,
+                contentDescription = "Next", colorFilter = ColorFilter.tint(White),
+                modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.CenterEnd)
+            )
+        }
+    }
+
+    @Composable
+    private fun NewFeature(newFeature: NewFeature) {
+        BoxWithConstraints(modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .background(Color.White)
+        ) {
+
+            Column(modifier = Modifier
+                .align(Alignment.TopCenter)
+                .matchParentSize()
+                .fillMaxHeight()) {
+                FeatureTitle(title = "New Feature")
+            }
+
+            Image(
+                imageVector = Icons.Filled.ArrowForwardIos,
+                contentDescription = "Next", colorFilter = ColorFilter.tint(White),
+                modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.CenterEnd)
+            )
+
+            Column(modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 16.dp)) {
+
+                ImageViaUrl(
+                    url = newFeature.imageUrl ?: "", modifier = Modifier
+                        .size(30.dp)
+                )
+
+                Text(
+                    text = newFeature.title ?: "",
+                    fontFamily = nunitoSans,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    color = Color.Black
+                )
+                Text(
+                    text = newFeature.description ?: "",
+                    fontFamily = nunitoSans,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun AdHocMessage(adHocMessage: AdHocMessage) {
+        BoxWithConstraints(modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .background(Color.White)
+        ) {
+
+            Column(modifier = Modifier
+                .align(Alignment.TopCenter)
+                .matchParentSize()
+                .fillMaxHeight()) {
+                FeatureTitle(title = "Ad Hoc Message")
+            }
+
+            Image(
+                imageVector = Icons.Filled.ArrowForwardIos,
+                contentDescription = "Next", colorFilter = ColorFilter.tint(White),
+                modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.CenterEnd)
+            )
+        }
+    }
+
+    @Composable
+    private fun FeatureTitle(title: String) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize()) {
+
+            Spacer(modifier = Modifier.weight(1f))
+            Box(modifier = Modifier
+                .padding(12.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(if (title != "") Color.Gray.copy(alpha = 0.5f) else Color.Transparent)) {
+                Text(text = title,
+                    fontFamily = nunitoSans,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 2.dp))
+            }
+        }
+
+    }
 
 }
