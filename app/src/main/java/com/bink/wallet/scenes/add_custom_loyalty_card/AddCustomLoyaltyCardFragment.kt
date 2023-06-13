6cac82cc3ea9e5863a13f968bbd488c39e314d33
Enoch Hankombo <ehankombo@bink.com>
2023-06-13 13:46:05 +0100

    Added fragment

---
+++ app/src/main/java/com/bink/wallet/scenes/add_custom_loyalty_card/AddCustomLoyaltyCardFragment.kt
@@ -0,0 +1,21 @@
+package com.bink.wallet.scenes.add_custom_loyalty_card
+
+import com.bink.wallet.BaseFragment
+import com.bink.wallet.databinding.AddLoyaltyCardFragmentBinding
+import com.bink.wallet.utils.toolbar.FragmentToolbar
+
+class AddCustomLoyaltyCardFragment :
+    BaseFragment<AddCustomLoyaltyCardViewModel, AddLoyaltyCardFragmentBinding>() {
+
+    override val layoutRes: Int
+        get() = TODO("Not yet implemented")
+    override val viewModel: AddCustomLoyaltyCardViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun builder(): FragmentToolbar {
+        TODO("Not yet implemented")
+    }
+
+
+
+}
---
+++ app/src/main/java/com/bink/wallet/scenes/add_custom_loyalty_card/AddCustomLoyaltyCardViewModel.kt
@@ -0,0 +1,6 @@
+package com.bink.wallet.scenes.add_custom_loyalty_card
+
+import com.bink.wallet.BaseViewModel
+
+class AddCustomLoyaltyCardViewModel: BaseViewModel() {
+}
