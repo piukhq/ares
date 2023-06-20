97e4076839101b3f6ca61ea3bef9636245ec72d3
Joshua Best <Jbest@bink.com>
2023-06-20 11:19:11 +0100

    Filtering new for you

--- app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsViewModel.kt
+++ app/src/main/java/com/bink/wallet/scenes/browse_brands/BrowseBrandsViewModel.kt
@@ -36,7 +36,7 @@ class BrowseBrandsViewModel : BaseViewModel() {
 
     fun setupBrandItems(
         membershipPlans: List<MembershipPlan>,
-        membershipCardIds: List<String>
+        membershipCardIds: List<String>,
     ) {
         val formattedBrandItems = formatBrandItemsList(
             membershipPlans.sortedByCardTypeAndCompany(),
@@ -82,15 +82,34 @@ class BrowseBrandsViewModel : BaseViewModel() {
 
     private fun formatBrandItemsList(
         membershipPlans: List<MembershipPlan>,
-        membershipCardIds: List<String>
+        membershipCardIds: List<String>,
     ): List<BrowseBrandsListItem> {
         val browseBrandsItems = mutableListOf<BrowseBrandsListItem>()
         val joinablePlans = membershipPlans
             .filter { plan -> plan.canPlanBeAdded() }
+        val newCards = joinablePlans.filter { it.isNewCard() }
         val (pllCards, RestOfCards) = joinablePlans.partition { it.isPlanPLL() }
 
         val (scrapableCards, storeCards) = RestOfCards.distinctBy { it.id }
             .partition { WebScrapableManager.isCardScrapable(it.id) }
+
+        if (newCards.isNotEmpty()) {
+            browseBrandsItems.add(
+                BrowseBrandsListItem.SectionTitleItem(
+                    R.string.new_card_title,
+                    null
+                )
+            )
+            newCards.forEachIndexed { index, membershipPlan ->
+                browseBrandsItems.add(
+                    BrowseBrandsListItem.BrandItem(
+                        membershipPlan,
+                        membershipPlan.id in membershipCardIds,
+                        isLastItem(index, newCards.size - 1)
+                    )
+                )
+            }
+        }
 
         if (pllCards.isNotEmpty()) {
             browseBrandsItems.add(
