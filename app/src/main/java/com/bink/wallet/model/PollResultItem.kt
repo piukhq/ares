15ae9619e5c84b844c814078954b76f6d74d0abb
Joshua Best <Jbest@bink.com>
2023-06-30 09:47:46 +0100

    Changed to documentId reference for firestore

--- app/src/main/java/com/bink/wallet/model/PollResultItem.kt
+++ app/src/main/java/com/bink/wallet/model/PollResultItem.kt
@@ -1,11 +1,14 @@
 package com.bink.wallet.model
 
+import com.google.firebase.firestore.DocumentId
+
 data class PollResultItem(
+    @DocumentId
+    val id: String? = null,
     val answer: String = "",
     val customAnswer: String = "",
     val createdDate: Int = 0,
     val overwritten: Boolean = false,
     val pollId: String? = null,
     val userId: String = "",
-    val id: String = ""
 )
