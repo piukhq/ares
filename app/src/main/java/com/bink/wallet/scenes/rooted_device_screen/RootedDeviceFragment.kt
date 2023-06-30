2158a5dbb461706f796761ff95667fd71c4f220b
Enoch Hankombo <ehankombo@bink.com>
2023-06-30 13:00:23 +0100

    Fixed rooted device screen

--- app/src/main/java/com/bink/wallet/scenes/rooted_device_screen/RootedDeviceFragment.kt
+++ app/src/main/java/com/bink/wallet/scenes/rooted_device_screen/RootedDeviceFragment.kt
@@ -1,19 +1,41 @@
 package com.bink.wallet.scenes.rooted_device_screen
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bink.wallet.R
+import com.bink.wallet.databinding.RootedDeviceFragmentBinding
 
 class RootedDeviceFragment : Fragment() {
+
+    private var _binding: RootedDeviceFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.rooted_device_fragment, container, false)
+        _binding = RootedDeviceFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.binkLogo.setImageResource(R.drawable.ic_logo_dark)
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                binding.binkLogo.setImageResource(R.drawable.ic_logo_light)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
